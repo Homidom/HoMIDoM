5660350b5b5773639fd993a30274ef7d90b08529
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-01-14 12:44:43 +0000

    Ajout du projet HomiDomService permettant de tester la solution, celui-ci est une simple application console qui sera remplacée par la suite par un service windows. C'est cette application qu'il faut lancer pour tester la solution.

---
+++ trunk/HoMIDomService/Service.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Runtime.Remoting
+Imports System.Runtime.Remoting.Channels
+Imports System.Runtime.Remoting.Channels.Http
+Imports System.Runtime.Remoting.Lifetime
+Imports HoMIDom.HoMIDom
+
+'***********************************************
+'** SERVICE HOMIDom - Simple exe qui sera ensuite convertit en service Windows
+'** Ce service créer le serveur SOAP Web via l'interface IHoMIDom
+'** Par défaut le service est lancé sur le port 8888
+'** version 1.0
+'** Date de création: 14/01/2011
+'** Historique (SebBergues): 14/01/2011: Création 
+'***********************************************
+
+Module Service
+    Dim obj As Server = New Server()
+
+    Sub Main()
+        Try
+
+            Console.WriteLine("******************************")
+            Console.WriteLine("DEMARRAGE DU SERVEUR**********")
+            Console.WriteLine("******************************")
+            Console.WriteLine(" ")
+
+            Console.WriteLine(Now & " Chargement de la configuration")
+            'Chargement de la config
+            'Console.WriteLine(Now & obj.LoadConfig("C:\ehome\config\"))
+
+            'Démarrage du serviceWeb
+            Console.WriteLine(Now & " ")
+            Console.WriteLine(Now & " Start ServiceWeb")
+            Dim chnl As New HttpChannel(8888) 'obj.PortTCP)
+            ChannelServices.RegisterChannel(chnl, False)
+            LifetimeServices.LeaseTime = Nothing
+            RemotingServices.Marshal(obj, "RemoteObjectServer.soap")
+            Console.WriteLine(Now & " ")
+            Console.WriteLine(Now & " ServiceWeb Démarré sur port:8888") ' & obj.PortTCP)
+
+            Console.WriteLine("******************************")
+            Console.WriteLine("SERVEUR DEMARRE **************")
+            Console.WriteLine("******************************")
+            Console.WriteLine(" ")
+
+            If Console.ReadLine = "end" Then
+                End
+            End If
+        Catch ex As Exception
+            Console.WriteLine(Now & " ERREUR " & ex.Message)
+        End Try
+    End Sub
+
+End Module
