4d497db4421406a0e314085cd7efc1d57db6a398
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-02-10 09:29:30 +0000

    Ajout squelettes des drivers Foobar, meteogoogle, k8055, k8056, openwebnet

---
+++ Driver_k8055/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Driver_k8055")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("cyberconseil")> 
+<Assembly: AssemblyProduct("Driver_k8055")> 
+<Assembly: AssemblyCopyright("Copyright © cyberconseil 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("0a6e9849-c5a1-4573-b098-256b5c9d3ac8")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Driver_k8056/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Driver_k8056")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("cyberconseil")> 
+<Assembly: AssemblyProduct("Driver_k8056")> 
+<Assembly: AssemblyCopyright("Copyright © cyberconseil 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("139f3ef9-78cf-4b23-85a5-d3a8acd6a88c")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
