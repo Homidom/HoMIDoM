8771d38a8a3e84e5a8a11114686107966ebd5ce8
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-02-24 10:22:15 +0000

    Correction bug start/stop Service Ajout fonctionnalités Test Device Admin

--- trunk/HoMIDom/APi.vb
+++ trunk/HoMIDom/APi.vb
@@ -24,6 +24,32 @@
             Next
             Return _Tabl
         End Function
+
+        'Liste les méthodes public d'un objet
+        'Nom_de_la_methode|parametre1:Type|parametre2:type...
+        Public Function ListMethod(ByVal Objet As Object) As ArrayList
+            Dim X As String
+            Dim Info As Reflection.MethodInfo
+            Dim paraminfos() As Object
+            Dim _Tabl As New ArrayList
+
+            For Each Info In Objet.GetType.GetMethods()
+                X = (Info.ReturnType.ToString) 'retourne le type string, boolean
+                If Info.Attributes = 6 And X = "System.Void" Then 'on prend que les méthodes public
+                    X = Info.Name 'Nom de la méthode
+                    paraminfos = Info.GetParameters()
+
+                    Dim tabl() As String
+                    For i As Integer = 0 To paraminfos.Count - 1
+                        tabl = paraminfos(i).ToString.Split(" ")
+                        X &= "|" & tabl(1) & ":" & tabl(0)
+                    Next
+
+                    _Tabl.Add(X)
+                End If
+            Next
+            Return _Tabl
+        End Function
     End Module
 
 End Namespace
