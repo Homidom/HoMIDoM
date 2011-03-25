929007f27324db17196c3096dcd1740351c2d82a
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-03-25 10:15:11 +0000

    Possibilité de configurer l'adresse du serveur (plusieurs adresses définissable dont 1 par défaut) puis sauvegarde dans un fichier de config
    Ajout des dernières version dans LastVersionstable

---
+++ trunk/HoMIDomAdminWpf/HoMIDomAdminWpf/ClServer.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Xml.Serialization
+
+<Serializable()> Public Class ClServer
+    Dim _Adresse As String
+    Dim _Defaut As Boolean
+
+    Public Property Adresse() As String
+        Get
+            Return _Adresse
+        End Get
+        Set(ByVal value As String)
+            _Adresse = value
+        End Set
+    End Property
+
+    Public Property Defaut() As Boolean
+        Get
+            Return _Defaut
+        End Get
+        Set(ByVal value As Boolean)
+            _Defaut = value
+        End Set
+    End Property
+
+
+End Class
