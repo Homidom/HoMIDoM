939b4a3c4a1cb27d9074564df7d8c53b3bb60690
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2011-04-06 16:01:30 +0000

    Manque encore l'admin.. je vais y arriver

---
+++ trunk/HoMIAdmiN/HoMIAdmiN/Control/uDevice.xaml.vb
@@ -0,0 +1,342 @@
+﻿Imports HoMIDom.HoMIDom.Device
+Imports HoMIDom.HoMIDom.Api
+Imports System.IO
+
+Partial Public Class uDevice
+    '--- Variables ------------------
+    Public Event CloseMe(ByVal MyObject As Object)
+    Dim _Action As EAction 'Définit si modif ou création d'un device
+    Dim _DeviceId As String 'Id du device à modifier
+    Dim FlagNewCmd As Boolean
+
+    Public Enum EAction
+        Nouveau
+        Modifier
+    End Enum
+
+    Public Sub New(ByVal Action As EAction, ByVal DeviceId As String)
+
+        ' Cet appel est requis par le Concepteur Windows Form.
+        InitializeComponent()
+        Try
+            ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+            _DeviceId = DeviceId
+            _Action = Action
+
+            'Liste les type de devices dans le combo
+            For Each value As ListeDevices In [Enum].GetValues(GetType(HoMIDom.HoMIDom.Device.ListeDevices))
+                CbType.Items.Add(value.ToString)
+            Next
+
+            'Liste les drivers dans le combo
+            For i As Integer = 0 To Window1.myService.GetAllDrivers.Count - 1 'Window1.Obj.Drivers.Count - 1
+                CbDriver.Items.Add(Window1.myService.GetAllDrivers.Item(i).Nom)  '(Window1.Obj.Drivers.Item(i).nom)
+            Next
+
+            If Action = EAction.Nouveau Then 'Nouveau Device
+                ImgDevice.Tag = " "
+            Else 'Modification d'un Device
+                Dim x As HoMIDom.HoMIDom.TemplateDevice = Window1.myService.ReturnDeviceByID(DeviceId) 'Window1.Obj.ReturnDeviceByID(DeviceId)
+
+                If x IsNot Nothing Then 'on a trouvé le device
+                    TxtNom.Text = x.Name
+                    TxtDescript.Text = x.description
+                    ChkEnable.IsChecked = x.Enable
+                    ChKSolo.IsChecked = x.Solo
+                    CbType.SelectedValue = x.Type.ToString
+                    CbType.IsEnabled = False
+
+                    For j As Integer = 0 To Window1.myService.GetAllDrivers.Count - 1 'Window1.Obj.Drivers.Count - 1
+                        If Window1.myService.GetAllDrivers.Item(j).ID = x.DriverId Then
+                            CbDriver.SelectedValue = Window1.myService.GetAllDrivers.Item(j).Nom
+                            Exit For
+                        End If
+                    Next
+
+                    TxtAdresse1.Text = x.adresse1
+                    TxtAdresse2.Text = x.adresse2
+                    TxtModele.Text = x.modele
+                    TxtRefresh.Text = x.refresh
+                    TxtLastChangeDuree.Text = x.LastChangeDuree
+
+                    If x.Picture <> "" And File.Exists(x.picture) = True Then
+                        Dim bmpImage As New BitmapImage()
+                        bmpImage.BeginInit()
+                        bmpImage.UriSource = New Uri(x.picture, UriKind.Absolute)
+                        bmpImage.EndInit()
+                        ImgDevice.Source = bmpImage
+                        ImgDevice.Tag = x.picture
+                    End If
+
+                    'Gestion si Device avec Value
+                    If x.Type = ListeDevices.TEMPERATURE _
+                                       Or x.Type = ListeDevices.HUMIDITE _
+                                       Or x.Type = ListeDevices.TEMPERATURECONSIGNE _
+                                       Or x.Type = ListeDevices.ENERGIETOTALE _
+                                       Or x.Type = ListeDevices.ENERGIEINSTANTANEE _
+                                       Or x.Type = ListeDevices.PLUIETOTAL _
+                                       Or x.Type = ListeDevices.PLUIECOURANT _
+                                       Or x.Type = ListeDevices.VITESSEVENT _
+                                       Or x.Type = ListeDevices.UV _
+                                       Or x.Type = ListeDevices.HUMIDITE _
+                                       Then
+                        TxtCorrection.Visibility = Windows.Visibility.Visible
+                        TxtCorrection.Text = x.Correction
+                        TxtFormatage.Visibility = Windows.Visibility.Visible
+                        TxtFormatage.Text = x.Formatage
+                        TxtPrecision.Visibility = Windows.Visibility.Visible
+                        TxtPrecision.Text = x.Precision
+                        TxtValueMax.Visibility = Windows.Visibility.Visible
+                        TxtValueMax.Text = x.ValueMax
+                        TxtValueMin.Visibility = Windows.Visibility.Visible
+                        TxtValueMin.Text = x.ValueMin
+                        TxtValDef.Visibility = Windows.Visibility.Visible
+                        TxtValDef.Text = x.ValueDef
+                        Label10.Visibility = Windows.Visibility.Visible
+                        Label11.Visibility = Windows.Visibility.Visible
+                        Label12.Visibility = Windows.Visibility.Visible
+                        Label13.Visibility = Windows.Visibility.Visible
+                        Label14.Visibility = Windows.Visibility.Visible
+                        Label15.Visibility = Windows.Visibility.Visible
+                    End If
+
+                    If x.Type = ListeDevices.MULTIMEDIA Then
+                        GroupBox1.Visibility = Windows.Visibility.Visible
+                        ListCmd.Items.Clear()
+                        For i As Integer = 0 To x.ListCommandName.Count - 1
+                            ListCmd.Items.Add(x.ListCommandName(i))
+                        Next
+                        x = Nothing
+                    Else
+                        GroupBox1.Visibility = Windows.Visibility.Hidden
+                    End If
+                End If
+            End If
+        Catch Ex As Exception
+            MessageBox.Show("Erreur: " & Ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    'Bouton Fermer
+    Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
+        RaiseEvent CloseMe(Me)
+    End Sub
+
+    'Bouton Ok
+    Private Sub BtnOK_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnOK.Click
+        If TxtNom.Text = "" Then
+            MessageBox.Show("Le nom du device est obligatoire !!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+            Exit Sub
+        End If
+        If CbType.Text = "" Then
+            MessageBox.Show("Le type du device est obligatoire !!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+            Exit Sub
+        End If
+        If CbDriver.Text = "" Then
+            MessageBox.Show("Le driver du device est obligatoire !!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+            Exit Sub
+        End If
+        If TxtAdresse1.Text = "" Then
+            MessageBox.Show("L'adresse de base du device est obligatoire !!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+            Exit Sub
+        End If
+
+        Dim _driverid As String = ""
+        For i As Integer = 0 To Window1.myService.GetAllDrivers.Count - 1 'Window1.Obj.Drivers.Count - 1
+            If Window1.myService.GetAllDrivers.Item(i).Nom = CbDriver.Text Then
+                _driverid = Window1.myService.GetAllDrivers.Item(i).ID
+                Exit For
+            End If
+        Next
+        Window1.myService.SaveDevice(_DeviceId, TxtNom.Text, TxtAdresse1.Text, ChkEnable.IsChecked, ChKSolo.IsChecked, _driverid, CbType.Text, TxtRefresh.Text, TxtAdresse2.Text, ImgDevice.Tag, TxtModele.Text, TxtDescript.Text, TxtLastChangeDuree.Text)
+        RaiseEvent CloseMe(Me)
+    End Sub
+
+    Private Sub TxtRefresh_TextChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.TextChangedEventArgs) Handles TxtRefresh.TextChanged
+        If TxtRefresh.Text <> "" And IsNumeric(TxtRefresh.Text) = False Then
+            MessageBox.Show("Veuillez saisir une valeur numérique")
+            TxtRefresh.Text = 0
+        End If
+    End Sub
+
+    Private Sub CbType_MouseLeave(ByVal sender As Object, ByVal e As System.Windows.Input.MouseEventArgs) Handles CbType.MouseLeave
+        TxtCorrection.Visibility = Windows.Visibility.Hidden
+        TxtFormatage.Visibility = Windows.Visibility.Hidden
+        TxtPrecision.Visibility = Windows.Visibility.Hidden
+        TxtValueMax.Visibility = Windows.Visibility.Hidden
+        TxtValueMin.Visibility = Windows.Visibility.Hidden
+        TxtValDef.Visibility = Windows.Visibility.Hidden
+        Label10.Visibility = Windows.Visibility.Hidden
+        Label11.Visibility = Windows.Visibility.Hidden
+        Label12.Visibility = Windows.Visibility.Hidden
+        Label13.Visibility = Windows.Visibility.Hidden
+        Label14.Visibility = Windows.Visibility.Hidden
+        Label15.Visibility = Windows.Visibility.Hidden
+
+        If _Action = EAction.Nouveau Then
+            'Gestion si Device avec Value
+            If CbType.SelectedValue Is Nothing Then Exit Sub
+            If CbType.SelectedValue = "TEMPERATURE" _
+                               Or CbType.Text = "HUMIDITE" _
+                               Or CbType.Text = "TEMPERATURECONSIGNE" _
+                               Or CbType.Text = "ENERGIETOTALE" _
+                               Or CbType.Text = "ENERGIEINSTANTANEE" _
+                               Or CbType.Text = "PLUIETOTAL" _
+                               Or CbType.Text = "PLUIECOURANT" _
+                               Or CbType.Text = "VITESSEVENT" _
+                               Or CbType.Text = "UV" _
+                               Then
+                TxtCorrection.Visibility = Windows.Visibility.Visible
+                TxtFormatage.Visibility = Windows.Visibility.Visible
+                TxtPrecision.Visibility = Windows.Visibility.Visible
+                TxtValueMax.Visibility = Windows.Visibility.Visible
+                TxtValueMin.Visibility = Windows.Visibility.Visible
+                TxtValDef.Visibility = Windows.Visibility.Visible
+                Label10.Visibility = Windows.Visibility.Visible
+                Label11.Visibility = Windows.Visibility.Visible
+                Label12.Visibility = Windows.Visibility.Visible
+                Label13.Visibility = Windows.Visibility.Visible
+                Label14.Visibility = Windows.Visibility.Visible
+                Label15.Visibility = Windows.Visibility.Visible
+            End If
+
+            If CbType.SelectedValue = "MULTIMEDIA" Then
+                GroupBox1.Visibility = Windows.Visibility.Visible
+            Else
+                GroupBox1.Visibility = Windows.Visibility.Hidden
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnNewCmd_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnNewCmd.Click
+        TxtCmdName.Text = ""
+        TxtCmdRepeat.Text = "0"
+        TxtCmdData.Text = ""
+
+        FlagNewCmd = True
+    End Sub
+
+    Private Sub BtnSaveCmd_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnSaveCmd.Click
+        If IsNumeric(TxtCmdRepeat.Text) = False Then
+            MsgBox("Numérique obligatoire pour repeat !!")
+            Exit Sub
+        End If
+        If TxtCmdName.Text = "" Or TxtCmdName.Text = " " Then
+            MsgBox("Le nom de la commande est obligatoire !!")
+            Exit Sub
+        End If
+
+        If FlagNewCmd = True Then 'nouvelle commande
+            Window1.myService.SaveDeviceCommandIR(_DeviceId, TxtCmdName.Text, TxtCmdData.Text, TxtCmdRepeat.Text)
+        Else 'modifier commande
+            Window1.myService.SaveDeviceCommandIR(_DeviceId, TxtCmdName.Text, TxtCmdData.Text, TxtCmdRepeat.Text)
+        End If
+
+        ListCmd.Items.Clear()
+        Dim x As Object = Window1.myService.ReturnDeviceByID(_DeviceId)
+        For i As Integer = 0 To x.listcommandname.count - 1
+            ListCmd.Items.Add(x.listcommandname(i))
+        Next
+        x = Nothing
+
+        FlagNewCmd = False
+    End Sub
+
+    Private Sub BtnDelCmd_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnDelCmd.Click
+        If ListCmd.SelectedIndex >= 0 Then
+            Window1.myService.DeleteDeviceCommandIR(_DeviceId, TxtCmdName.Text)
+
+            TxtCmdName.Text = ""
+            TxtCmdData.Text = ""
+            TxtCmdRepeat.Text = "0"
+            ListCmd.Items.Clear()
+
+            Dim x As Object = Window1.myService.ReturnDeviceByID(_DeviceId)
+            For i As Integer = 0 To x.listcommandname.count - 1
+                ListCmd.Items.Add(x.listcommandname(i))
+            Next
+            x = Nothing
+
+        End If
+    End Sub
+
+    Private Sub BtnTstCmd_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnTstCmd.Click
+        Dim _Param As New ArrayList
+        Dim x As New HoMIDom.HoMIDom.DeviceAction
+        Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+
+        y.Value = TxtCmdName.Text
+
+        With x
+            .Nom = "SendCommand"
+            .Parametres.Add(y)
+        End With
+
+        Window1.myService.ExecuteDeviceCommand(_DeviceId, x)
+    End Sub
+
+    Private Sub BtnLearn_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnLearn.Click
+        TxtCmdData.Text = Window1.myService.StartIrLearning
+    End Sub
+
+    Private Sub TxtLastChangeDuree_TextChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.TextChangedEventArgs) Handles TxtLastChangeDuree.TextChanged
+        If TxtLastChangeDuree.Text <> "" And IsNumeric(TxtLastChangeDuree.Text) = False Then
+            MessageBox.Show("Veuillez saisir une valeur numérique")
+            TxtLastChangeDuree.Text = 0
+        End If
+    End Sub
+
+    Private Sub ImgDevice_MouseLeftButtonDown(ByVal sender As Object, ByVal e As System.Windows.Input.MouseButtonEventArgs) Handles ImgDevice.MouseLeftButtonDown
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+        dlg.Filter = "jpeg (*.jpg) |*.jpg;*.jpeg|(*.png) |*.png|(*.*) |*.*"
+
+        If dlg.ShowDialog() = True Then
+            Dim bmpImage As New BitmapImage()
+            bmpImage.BeginInit()
+            bmpImage.UriSource = New Uri(dlg.FileName, UriKind.Absolute)
+            bmpImage.EndInit()
+            ImgDevice.Source = bmpImage
+            ImgDevice.Tag = dlg.FileName
+        End If
+    End Sub
+
+    Private Sub ListCmd_SelectionChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.SelectionChangedEventArgs) Handles ListCmd.SelectionChanged
+        If ListCmd.SelectedIndex < 0 Then Exit Sub
+        TxtCmdName.Text = Window1.myService.ReturnDeviceByID(_DeviceId).ListCommandName(ListCmd.SelectedIndex)
+        TxtCmdData.Text = Window1.myService.ReturnDeviceByID(_DeviceId).ListCommandData(ListCmd.SelectedIndex)
+        TxtCmdRepeat.Text = Window1.myService.ReturnDeviceByID(_DeviceId).ListCommandRepeat(ListCmd.SelectedIndex)
+    End Sub
+
+    Private Sub BtnRead_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnRead.Click
+
+        Try
+            Dim y As New uTestDevice(_DeviceId)
+            y.Uid = System.Guid.NewGuid.ToString()
+            AddHandler y.CloseMe, AddressOf UnloadControl
+            Window1.CanvasUser.Children.Add(y)
+            Window1.CanvasUser.SetLeft(y, 50)
+            Window1.CanvasUser.SetTop(y, 50)
+        Catch ex As Exception
+            MessageBox.Show("Erreur Tester: " & ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    'Private Sub BtnWrite_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+    '    Try
+    '        Window1.Obj.TestWrite(_DeviceId, TxtCmd.Text, TxtP1.Text, TxtP2.Text)
+    '    Catch ex As Exception
+    '        MessageBox.Show("Error Write device" & ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error)
+    '    End Try
+    'End Sub
+
+    Private Sub UnloadControl(ByVal MyControl As Object)
+        For i As Integer = 0 To Window1.CanvasUser.Children.Count - 1
+            If Window1.CanvasUser.Children.Item(i).Uid = MyControl.uid Then
+                Window1.CanvasUser.Children.RemoveAt(i)
+                Exit Sub
+            End If
+        Next
+
+    End Sub
+
+End Class
---
+++ trunk/HoMIAdmiN/HoMIAdmiN/Control/uDriver.xaml.vb
@@ -0,0 +1,202 @@
+﻿Imports HoMIDom.HoMIDom.Device
+Imports HoMIDom.HoMIDom.Api
+Imports System.IO
+
+Partial Public Class uDriver
+    '--- Variables ------------------
+    Public Event CloseMe(ByVal MyObject As Object)
+    Dim _DriverId As String 'Id du device à modifier
+    Dim x As HoMIDom.HoMIDom.TemplateDriver
+
+    Public Sub New(ByVal DriverId As String)
+
+        ' Cet appel est requis par le Concepteur Windows Form.
+        InitializeComponent()
+        Try
+            ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+            _DriverId = DriverId
+
+            x = Window1.myService.ReturnDriverByID(DriverId) 'Window1.Obj.ReturnDeviceByID(DeviceId)
+
+            If x IsNot Nothing Then 'on a trouvé le device
+                TxtNom.Text = x.Nom
+                TxtDescript.Text = x.Description
+                ChkEnable.IsChecked = x.Enable
+                CbStartAuto.IsChecked = x.StartAuto
+                TxtProtocol.Text = x.Protocol
+                TxtAdrTCP.Text = x.IP_TCP
+                TxtPortTCP.Text = x.Port_TCP
+                TxtAdrUDP.Text = x.IP_UDP
+                TxtPortUDP.Text = x.Port_UDP
+                TxtCom.Text = x.COM
+                TxtRefresh.Text = x.Refresh
+                TxtVersion.Text = x.Version
+                TxtModele.Text = x.Modele
+
+                If x.Picture <> "" And File.Exists(x.Picture) = True Then
+                    Dim bmpImage As New BitmapImage()
+                    bmpImage.BeginInit()
+                    bmpImage.UriSource = New Uri(x.Picture, UriKind.Absolute)
+                    bmpImage.EndInit()
+                    ImgDevice.Source = bmpImage
+                    ImgDevice.Tag = x.Picture
+                End If
+
+            End If
+
+        Catch Ex As Exception
+            MessageBox.Show("Erreur: " & Ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    'Bouton Fermer
+    Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
+        RaiseEvent CloseMe(Me)
+    End Sub
+
+    'Bouton Ok
+    Private Sub BtnOK_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnOK.Click
+        Window1.myService.SaveDriver(_DriverId, TxtNom.Text, ChkEnable.IsChecked, CbStartAuto.IsChecked, TxtAdrTCP.Text, TxtPortTCP.Text, TxtAdrUDP.Text, TxtPortUDP.Text, TxtCom.Text, TxtRefresh.Text, ImgDevice.Tag)
+        RaiseEvent CloseMe(Me)
+    End Sub
+
+    Private Sub TxtRefresh_TextChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.TextChangedEventArgs) Handles TxtRefresh.TextChanged
+        If TxtRefresh.Text <> "" And IsNumeric(TxtRefresh.Text) = False Then
+            MessageBox.Show("Veuillez saisir une valeur numérique")
+            TxtRefresh.Text = 0
+        End If
+    End Sub
+
+
+    Private Sub ImgDevice_MouseLeftButtonDown(ByVal sender As Object, ByVal e As System.Windows.Input.MouseButtonEventArgs) Handles ImgDevice.MouseLeftButtonDown
+        Dim dlg As New Microsoft.Win32.OpenFileDialog()
+        dlg.Filter = "jpeg (*.jpg) |*.jpg;*.jpeg|(*.png) |*.png|(*.*) |*.*"
+
+        If dlg.ShowDialog() = True Then
+            Dim bmpImage As New BitmapImage()
+            bmpImage.BeginInit()
+            bmpImage.UriSource = New Uri(dlg.FileName, UriKind.Absolute)
+            bmpImage.EndInit()
+            ImgDevice.Source = bmpImage
+            ImgDevice.Tag = dlg.FileName
+        End If
+    End Sub
+
+    Private Sub UnloadControl(ByVal MyControl As Object)
+        For i As Integer = 0 To Window1.CanvasUser.Children.Count - 1
+            If Window1.CanvasUser.Children.Item(i).Uid = MyControl.uid Then
+                Window1.CanvasUser.Children.RemoveAt(i)
+                Exit Sub
+            End If
+        Next
+
+    End Sub
+
+    Private Sub BtnAv_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnAv.Click
+        If GroupBox1.Visibility = Windows.Visibility.Hidden Then
+            BtnAv.Content = "<<"
+            GroupBox1.Visibility = Windows.Visibility.Visible
+            CbCmd.Items.Clear()
+            If x IsNot Nothing Then
+                For i As Integer = 0 To x.DeviceAction.Count - 1
+                    CbCmd.Items.Add(x.DeviceAction.Item(i).Nom)
+                Next
+            End If
+        Else
+            CbCmd.Items.Clear()
+            BtnAv.Content = ">>"
+            GroupBox1.Visibility = Windows.Visibility.Hidden
+        End If
+    End Sub
+
+    Private Sub CbCmd_MouseLeave(ByVal sender As Object, ByVal e As System.Windows.Input.MouseEventArgs) Handles CbCmd.MouseLeave
+        LblP1.Visibility = Windows.Visibility.Hidden
+        LblP2.Visibility = Windows.Visibility.Hidden
+        LblP3.Visibility = Windows.Visibility.Hidden
+        LblP4.Visibility = Windows.Visibility.Hidden
+        LblP5.Visibility = Windows.Visibility.Hidden
+        TxtP1.Text = ""
+        TxtP2.Text = ""
+        TxtP3.Text = ""
+        TxtP4.Text = ""
+        TxtP5.Text = ""
+        TxtP1.Visibility = Windows.Visibility.Hidden
+        TxtP2.Visibility = Windows.Visibility.Hidden
+        TxtP3.Visibility = Windows.Visibility.Hidden
+        TxtP4.Visibility = Windows.Visibility.Hidden
+        TxtP5.Visibility = Windows.Visibility.Hidden
+
+        If CbCmd.Text = "" Then Exit Sub
+        If CbCmd.SelectedIndex < 0 Then Exit Sub
+
+        Dim Idx As Integer = CbCmd.SelectedIndex
+        For j As Integer = 0 To x.DeviceAction.Item(Idx).Parametres.Count - 1
+
+            Select Case j
+                Case 0
+                    LblP1.Content = x.DeviceAction.Item(Idx).Parametres.Item(j).Nom & " :"
+                    LblP1.Visibility = Windows.Visibility.Visible
+                    TxtP1.ToolTip = x.DeviceAction.Item(Idx).Parametres.Item(j).Type
+                    TxtP1.Visibility = Windows.Visibility.Visible
+                Case 1
+                    LblP2.Content = x.DeviceAction.Item(Idx).Parametres.Item(j).Nom & " :"
+                    LblP2.Visibility = Windows.Visibility.Visible
+                    TxtP2.ToolTip = x.DeviceAction.Item(Idx).Parametres.Item(j).Type
+                    TxtP2.Visibility = Windows.Visibility.Visible
+                Case 2
+                    LblP3.Content = x.DeviceAction.Item(Idx).Parametres.Item(j).Nom & " :"
+                    LblP3.Visibility = Windows.Visibility.Visible
+                    TxtP3.ToolTip = x.DeviceAction.Item(Idx).Parametres.Item(j).Type
+                    TxtP3.Visibility = Windows.Visibility.Visible
+                Case 3
+                    LblP4.Content = x.DeviceAction.Item(Idx).Parametres.Item(j).Nom & " :"
+                    LblP4.Visibility = Windows.Visibility.Visible
+                    TxtP4.ToolTip = x.DeviceAction.Item(Idx).Parametres.Item(j).Type
+                    TxtP4.Visibility = Windows.Visibility.Visible
+                Case 4
+                    LblP5.Content = x.DeviceAction.Item(Idx).Parametres.Item(j).Nom & " :"
+                    LblP5.Visibility = Windows.Visibility.Visible
+                    TxtP5.ToolTip = x.DeviceAction.Item(Idx).Parametres.Item(j).Type
+                    TxtP5.Visibility = Windows.Visibility.Visible
+            End Select
+        Next
+    End Sub
+
+    Private Sub BtnTest_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnTest.Click
+        Try
+            Dim a As New HoMIDom.HoMIDom.DeviceAction
+
+            a.Nom = CbCmd.Text
+
+            If TxtP1.Text <> "" Then
+                Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+                y.Value = TxtP1.Text
+                a.Parametres.Add(y)
+            End If
+            If TxtP2.Text <> "" Then
+                Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+                y.Value = TxtP2.Text
+                a.Parametres.Add(y)
+            End If
+            If TxtP3.Text <> "" Then
+                Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+                y.Value = TxtP3.Text
+                a.Parametres.Add(y)
+            End If
+            If TxtP4.Text <> "" Then
+                Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+                y.Value = TxtP4.Text
+                a.Parametres.Add(y)
+            End If
+            If TxtP5.Text <> "" Then
+                Dim y As New HoMIDom.HoMIDom.DeviceAction.Parametre
+                y.Value = TxtP5.Text
+                a.Parametres.Add(y)
+            End If
+
+            Window1.myService.ExecuteDriverCommand(_DriverId, a)
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors du test: " & ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+End Class
