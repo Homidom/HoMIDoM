d2cd6b72198570018375126c8a751eaf1c3339d4
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2011-04-15 22:29:56 +0000

    code des macros/trigger

--- trunk/HoMIDom/macro.vb
+++ trunk/HoMIDom/macro.vb
@@ -1,89 +1,38 @@
 ﻿Imports HoMIDom
 Imports HoMIDom.HoMIDom.Server
+Imports NCrontab
+Imports STRGS = Microsoft.VisualBasic.Strings
 
 Namespace HoMIDom
 
     ''' <summary>Class Macro, Défini le type pour les macros</summary>
     ''' <remarks>_condition contient un tableau des conditions à vérifier pour lancer les actions si TRUE ou si FALSE</remarks>
-    Public Class macro
+    Public Class Macro
 
-        Dim _ID As String
-        Dim _Nom As String
-        Dim _Description As String
-        Dim _Enable As Boolean
-        Dim _Condition As ArrayList
-        Dim _ActionTrue As ArrayList
-        Dim _ActionFalse As ArrayList
+        Dim ID As String
+        Dim Nom As String
+        Dim Description As String
+        Dim Enable As Boolean
+        Dim Condition As ArrayList
+        Dim ActionTrue As ArrayList
+        Dim ActionFalse As ArrayList
 
         Dim _Server As Server
         Dim _Device As Device
-
-        Public ReadOnly Property ID As String
-            Get
-                Return _ID
-            End Get
-        End Property
-        Public Property Nom As String
-            Get
-                Return _Nom
-            End Get
-            Set(ByVal value As String)
-                _Nom = value
-            End Set
-        End Property
-        Public Property Description As String
-            Get
-                Return _Description
-            End Get
-            Set(ByVal value As String)
-                _Description = value
-            End Set
-        End Property
-        Public Property Enable As Boolean
-            Get
-                Return _Enable
-            End Get
-            Set(ByVal value As Boolean)
-                _Enable = value
-            End Set
-        End Property
-        Public Property Condition As ArrayList
-            Get
-                Return _Condition
-            End Get
-            Set(ByVal value As ArrayList)
-                _Condition = value
-            End Set
-        End Property
-        Public Property ActionTrue As ArrayList
-            Get
-                Return _ActionTrue
-            End Get
-            Set(ByVal value As ArrayList)
-                _ActionTrue = value
-            End Set
-        End Property
-        Public Property ActionFalse As ArrayList
-            Get
-                Return _ActionFalse
-            End Get
-            Set(ByVal value As ArrayList)
-                _ActionFalse = value
-            End Set
-        End Property
 
         ''' <summary>Execute une macro avec analyse des conditions</summary>
         ''' <remarks>lance les actions True ou False suivant le résultat des conditions</remarks>
         Public Sub Execute_avec_conditions()
             Try
+                _Server.Log(TypeLog.DEBUG, TypeSource.SERVEUR, "Macro:Execute_avec_conditions", "Execution avec tests des conditions de " & Nom)
                 'analyse des conditions
                 If Analyse() = True Then
                     'actionsTRUE
-                    If _ActionTrue.Count <> 0 Then
-                        Action(_ActionTrue)
+                    If ActionTrue.Count <> 0 Then
+                        Action(ActionTrue)
                     End If
-                ElseIf _ActionFalse.Count <> 0 Then
-                    Action(_ActionFalse)
+                ElseIf ActionFalse.Count <> 0 Then
+                    Action(ActionFalse)
                 End If
             Catch ex As Exception
                 _Server.Log(TypeLog.ERREUR, TypeSource.SERVEUR, "Macro:Execute_avec_conditions", ex.ToString)
@@ -94,8 +43,10 @@ Namespace HoMIDom
         ''' <remarks>lance les actions si TRUE</remarks>
         Public Sub Execute_sans_conditions()
             Try
-                If _ActionTrue.Count <> 0 Then
-                    Action(_ActionTrue)
+                _Server.Log(TypeLog.DEBUG, TypeSource.SERVEUR, "Macro:Execute_sans_conditions", "Execution sans tests des conditions de " & Nom)
+                'on ne teste pas, on lance direct les actions de True
+                If ActionTrue.Count <> 0 Then
+                    Action(ActionTrue)
                 End If
             Catch ex As Exception
                 _Server.Log(TypeLog.ERREUR, TypeSource.SERVEUR, "Macro:Execute_sans_conditions", ex.ToString)
@@ -106,7 +57,10 @@ Namespace HoMIDom
         ''' <remarks></remarks>
         Public Function Analyse() As Boolean
             Try
+                _Server.Log(TypeLog.DEBUG, TypeSource.SERVEUR, "Macro:Analyse", "Analyse des conditions de : " & Nom)
 
+
+                'Ajouter le code Ncalc ou autre
 
 
                 Return True
@@ -123,9 +77,11 @@ Namespace HoMIDom
         ''' <remarks></remarks>
         Public Sub Action(ByVal listeactions As ArrayList)
             Try
+                For i = 0 To listeactions.Count - 1
+                    _Server.Log(TypeLog.DEBUG, TypeSource.SERVEUR, "Macro:Action", "Execution de l'action : " & listeactions.Item(i))
 
 
-
+                Next
             Catch ex As Exception
                 _Server.Log(TypeLog.ERREUR, TypeSource.SERVEUR, "Macro:Action", ex.ToString)
             End Try
@@ -139,67 +95,21 @@ Namespace HoMIDom
     ''' </remarks>
     Public Class trigger
 
-        Dim _ID As String
-        Dim _Nom As String
-        Dim _Description As String
-        Dim _Enable As Boolean
-        Dim _Condition As ArrayList
-        Dim _Macro As ArrayList
+        Public ID As String
+        Public Nom As String
+        Public Description As String
+        Public Enable As Boolean
+        Public Condition As ArrayList
+        Public Macro As ArrayList
 
-        Dim _Server As Server
-
-        Public ReadOnly Property ID As String
-            Get
-                Return _ID
-            End Get
-        End Property
-        Public Property Nom As String
-            Get
-                Return _Nom
-            End Get
-            Set(ByVal value As String)
-                _Nom = value
-            End Set
-        End Property
-        Public Property Description As String
-            Get
-                Return _Description
-            End Get
-            Set(ByVal value As String)
-                _Description = value
-            End Set
-        End Property
-        Public Property Enable As Boolean
-            Get
-                Return _Enable
-            End Get
-            Set(ByVal value As Boolean)
-                _Enable = value
-            End Set
-        End Property
-        Public Property Condition As ArrayList
-            Get
-                Return _Condition
-            End Get
-            Set(ByVal value As ArrayList)
-                _Condition = value
-            End Set
-        End Property
-        Public Property Macro As ArrayList
-            Get
-                Return _Macro
-            End Get
-            Set(ByVal value As ArrayList)
-                _Macro = value
-            End Set
-        End Property
+        Public _Server As Server
 
         ''' <summary>Analyse si un device fait parti des conditions</summary>
         ''' <remarks></remarks>
         Public Function Analyse(ByVal DeviceId As String) As Boolean
             Try
-                For i = 0 To _Condition.Count - 1
-                    If _Condition.Item(0) = DeviceId Then Return True
+                For i = 0 To Condition.Count - 1
+                    If Condition.Item(0) = DeviceId Then Return True
                 Next
                 'device non trouvé dans la liste
                 Return False
@@ -209,6 +119,40 @@ Namespace HoMIDom
             End Try
         End Function
 
+    End Class
+
+    ''' <summary>Class triggercron,permet uniquement d'etre utiliser dans le timer interne au server</summary>
+    ''' <remarks></remarks>
+    Public Class triggercron
+        Public cron As String 'Le cron du trigger
+        Public prochainedateheure As DateTime 'la date/heure de prochaine execution
+        Public TriggerID As String 'l'id du trigger pour lancer les macros associés
+
+        Public _Server As Server
+
+        ''' <summary>Converti un cron en prochaine date/heure d'execution</summary>
+        ''' <remarks></remarks>
+        Public Sub maj_cron()
+            'convertit la condition au format cron "cron_ss#mm#hh#jj#MMM#JJJ" en dateTime
+            Try
+                Dim conditions = STRGS.Split(cron, "#")
+
+                'Dim s = CrontabSchedule.Parse("0 17-19 * * *")
+                Dim s = CrontabSchedule.Parse(conditions(1) & " " & conditions(2) & " " & conditions(3) & " " & conditions(4) & " " & conditions(5))
+                'recupere le prochain shedule
+                Dim nextcron = s.GetNextOccurrence(DateAndTime.Now)
+                If (conditions(0) <> "*" And conditions(0) <> "") Then nextcron = nextcron.AddSeconds(conditions(0))
+                cron = nextcron.ToString("yyyy-MM-dd HH:mm:ss")
+
+                'recupere la liste des prochains shedule
+                'Dim nextcron = s.GetNextOccurrences(DateAndTime.Now, DateAndTime.Now.AddDays(1))
+                'For Each i In nextcron
+                '    MsgBox(i.ToString("yyyy-MM-dd HH:mm:ss"))
+                'Next
+            Catch ex As Exception
+                _Server.Log(TypeLog.ERREUR, TypeSource.SERVEUR, "Trigger:cron_convertendate", ex.ToString)
+            End Try
+        End Sub
     End Class
 
 End Namespace
