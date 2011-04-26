2e6cb13fd8e3d9f3cbb729b833b6adc05f2f9268
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-04-26 07:49:21 +0000

    Renvoi -1 si erreur lors du delete ou Save (Device, zone, driver...) Possibilité de paramétrer le paramètre maxfilesizelog depuis Admin

--- HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
@@ -18,9 +18,20 @@
 
         ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
         RefreshLog()
+
+        If Window1.IsConnect = True Then
+            TxtFile.Text = Window1.myService.GetMaxFileSizeLog
+        End If
     End Sub
 
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
+        If IsNumeric(TxtFile.Text) = False Or CDbl(TxtFile.Text) < 1 Then
+            MessageBox.Show("Veuillez saisir un numérique et positif pour la taille max du fichier de log!", "Admin", MessageBoxButton.OK, MessageBoxImage.Exclamation)
+            Exit Sub
+        End If
+        If Window1.IsConnect = True And IsNumeric(TxtFile.Text) Then
+            Window1.myService.SetMaxFileSizeLog(CDbl(TxtFile.Text))
+        End If
         RaiseEvent CloseMe(Me)
     End Sub
 End Class
