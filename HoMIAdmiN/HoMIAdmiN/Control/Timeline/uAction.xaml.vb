510a0f0910156502d4fd63cc453e6fa00a84548c
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-06-15 19:59:59 +0000

    Modification majeure dans la gestion des macros/actions dans le service ce qui facilite largement leurs gestions côté client/admin Test concluant à continuer...

--- HoMIAdmiN/HoMIAdmiN/Control/Timeline/uAction.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/Timeline/uAction.xaml.vb
@@ -1,10 +1,11 @@
 ﻿Public Class uAction
     Dim _TypeAction As HoMIDom.HoMIDom.Action.TypeAction
-    Dim _Libelle As String
-    Dim _Action As String
-    Dim _IDdevice As String
+    'Dim _Libelle As String
+    'Dim _Action As String
+    'Dim _IDdevice As String
     Dim _Timing As DateTime
-    Dim _Parametres As New ArrayList
+    'Dim _Parametres As New ArrayList
+    Dim _ObjAction As Object
     Public Span As Integer
     Public _Zoom As Integer
 
@@ -28,18 +29,18 @@
         Select Case _TypeAction
             Case HoMIDom.HoMIDom.Action.TypeAction.ActionDevice 'Action Device
                 Dim frm As New WActionDevice
-                If IDDevice <> "" Then
-                    frm.ID = IDDevice
-                    frm.Action = Action
-                    frm.Delai = Timing
-                    frm.Parametres = Parametres
-                End If
+                'If IDDevice <> "" Then
+                'frm.ID = IDDevice
+                'frm.Action = Action
+                'frm.Delai = Timing
+                'frm.Parametres = Parametres
+                'End If
                 frm.ShowDialog()
                 If frm.DialogResult.HasValue And frm.DialogResult.Value Then
-                    IDDevice = frm.ID
-                    Action = frm.Action
+                    'IDDevice = frm.ID
+                    'Action = frm.Action
                     Timing = frm.Delai
-                    Parametres = frm.Parametres
+                    'Parametres = frm.Parametres
                     frm.Close()
                     RaiseEvent ChangeAction(Me.Uid)
                 End If
@@ -47,6 +48,25 @@
 
         End Select
     End Sub
+
+    Public Property ObjAction As Object
+        Get
+            Return _ObjAction
+        End Get
+        Set(ByVal value As Object)
+            _ObjAction = value
+            If _ObjAction IsNot Nothing Then
+                Select Case _ObjAction.TypeAction
+                    Case 0 'Action device
+                        Dim x As HoMIDom.HoMIDom.Action.ActionDevice
+                        x = _ObjAction
+                        Label1.Content = Window1.myService.ReturnDeviceByID(x.IdDevice).Name
+                    Case 1 'Action mail
+
+                End Select
+            End If
+        End Set
+    End Property
 
     Public Property Timing As DateTime
         Get
@@ -61,15 +81,15 @@
         End Set
     End Property
 
-    Public Property IDDevice As String
-        Get
-            Return _IDdevice
-        End Get
-        Set(ByVal value As String)
-            _IDdevice = value
-            Libelle = Window1.myService.ReturnDeviceByID(_IDdevice).Name
-        End Set
-    End Property
+    'Public Property IDDevice As String
+    '    Get
+    '        Return _IDdevice
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _IDdevice = value
+    '        Libelle = Window1.myService.ReturnDeviceByID(_IDdevice).Name
+    '    End Set
+    'End Property
 
     Public Property TypeAction As Integer
         Get
@@ -80,34 +100,34 @@
         End Set
     End Property
 
-    Public Property Libelle As String
-        Get
-            Return _Libelle
-        End Get
-        Set(ByVal value As String)
-            _Libelle = value
-            Label1.Content = value
-        End Set
-    End Property
+    'Public Property Libelle As String
+    '    Get
+    '        Return _Libelle
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _Libelle = value
+    '        Label1.Content = value
+    '    End Set
+    'End Property
 
-    Public Property Action As String
-        Get
-            Return _Action
-        End Get
-        Set(ByVal value As String)
-            _Action = value
-            Label2.Content = value
-        End Set
-    End Property
+    'Public Property Action As String
+    '    Get
+    '        Return _Action
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _Action = value
+    '        Label2.Content = value
+    '    End Set
+    'End Property
 
-    Public Property Parametres As ArrayList
-        Get
-            Return _Parametres
-        End Get
-        Set(ByVal value As ArrayList)
-            _Parametres = value
-        End Set
-    End Property
+    'Public Property Parametres As ArrayList
+    '    Get
+    '        Return _Parametres
+    '    End Get
+    '    Set(ByVal value As ArrayList)
+    '        _Parametres = value
+    '    End Set
+    'End Property
 
     Private Sub ImgDelete_MouseLeftButtonDown(ByVal sender As Object, ByVal e As System.Windows.Input.MouseButtonEventArgs) Handles ImgDelete.MouseLeftButtonDown
         RaiseEvent DeleteAction(Me.Uid)
--- HoMIAdmiN/HoMIAdmiN/Control/uMacro.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uMacro.xaml.vb
@@ -25,17 +25,13 @@
         If _Action = EAction.Nouveau Then 'Nouvelle macro
 
         Else 'Modifier Macro
-            Dim x As HoMIDom.HoMIDom.TemplateMacro = Window1.myService.ReturnMacroById(MacroId)
+            Dim x As HoMIDom.HoMIDom.Macro = Window1.myService.ReturnMacroById(MacroId)
 
             If x IsNot Nothing Then
                 TxtNom.Text = x.Nom
                 TxtDescription.Text = x.Description
                 cEnable.IsChecked = x.Enable
-                Dim tabl As New ArrayList
-                For j As Integer = 0 To x.ListActions.Count - 1
-                    tabl.Add(x.ListActions.Item(j))
-                Next
-                UScenario1.Items = tabl
+                UScenario1.Items = x.ListActions
             End If
         End If
     End Sub
@@ -48,37 +44,37 @@
 
         If _Action = EAction.Nouveau Then
             Dim tabl As New ArrayList
-            Dim tabl2 As New List(Of HoMIDom.HoMIDom.TemplateAction)
+            'Dim tabl2 As New List(Of HoMIDom.HoMIDom.TemplateAction)
 
             tabl = UScenario1.Items
-            For i As Integer = 0 To tabl.Count - 1
-                Select Case tabl.Item(i).TypeAction
-                    Case HoMIDom.HoMIDom.Action.TypeAction.ActionDevice
-                        Dim o As New HoMIDom.HoMIDom.TemplateAction
-                        o.Timing = tabl.Item(i).timing
-                        o.IdDevice = tabl.Item(i).IdDevice
-                        o.Action = tabl.Item(i).Method
-                        o.Parametres = tabl.Item(i).Parametres
-                        tabl2.Add(o)
-                End Select
-            Next
-            Window1.myService.SaveMacro("", TxtNom.Text, cEnable.IsChecked, TxtDescription.Text, tabl2)
+            'For i As Integer = 0 To tabl.Count - 1
+            '    Select Case tabl.Item(i).TypeAction
+            '        Case HoMIDom.HoMIDom.Action.TypeAction.ActionDevice
+            '            Dim o As New HoMIDom.HoMIDom.TemplateAction
+            '            o.Timing = tabl.Item(i).timing
+            '            o.IdDevice = tabl.Item(i).IdDevice
+            '            o.Action = tabl.Item(i).Method
+            '            o.Parametres = tabl.Item(i).Parametres
+            '            tabl2.Add(o)
+            '    End Select
+            'Next
+            Window1.myService.SaveMacro("", TxtNom.Text, cEnable.IsChecked, TxtDescription.Text, tabl)
         Else
             Dim tabl As New ArrayList
-            Dim tabl2 As New List(Of HoMIDom.HoMIDom.TemplateAction)
-            tabl = UScenario1.Items
-            For i As Integer = 0 To tabl.Count - 1
-                Select Case tabl.Item(i).TypeAction
-                    Case HoMIDom.HoMIDom.Action.TypeAction.ActionDevice
-                        Dim o As New HoMIDom.HoMIDom.TemplateAction
-                        o.Timing = tabl.Item(i).Timing
-                        o.IdDevice = tabl.Item(i).IdDevice
-                        o.Action = tabl.Item(i).method
-                        o.Parametres = tabl.Item(i).Parametres
-                        tabl2.Add(o)
-                End Select
-            Next
-            Window1.myService.SaveMacro(_MacroId, TxtNom.Text, cEnable.IsChecked, TxtDescription.Text, tabl2)
+            'Dim tabl2 As New List(Of HoMIDom.HoMIDom.TemplateAction)
+            'tabl = UScenario1.Items
+            'For i As Integer = 0 To tabl.Count - 1
+            '    Select Case tabl.Item(i).TypeAction
+            '        Case HoMIDom.HoMIDom.Action.TypeAction.ActionDevice
+            '            Dim o As New HoMIDom.HoMIDom.TemplateAction
+            '            o.Timing = tabl.Item(i).Timing
+            '            o.IdDevice = tabl.Item(i).IdDevice
+            '            o.Action = tabl.Item(i).method
+            '            o.Parametres = tabl.Item(i).Parametres
+            '            tabl2.Add(o)
+            '    End Select
+            'Next
+            Window1.myService.SaveMacro(_MacroId, TxtNom.Text, cEnable.IsChecked, TxtDescription.Text, tabl)
         End If
         RaiseEvent CloseMe(Me)
     End Sub
