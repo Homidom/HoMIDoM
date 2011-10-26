6a3220ffcff1c5be44a21f7a6b906aaadacb649e
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-10-26 16:16:35 +0000

    Amélioration perf macro Ajout relevés

---
+++ trunk/HoMIAdmiN/HoMIAdmiN/Control/uReleve.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports HoMIDom.HoMIDom
+
+Public Class uReleve
+    Dim _listhisto As New List(Of Historisation)
+    Public Event CloseMe(ByVal MyObject As Object)
+
+    Public Sub New(ByVal ListReleve)
+
+        ' Cet appel est requis par le concepteur.
+        InitializeComponent()
+
+        ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+        _listhisto = ListReleve
+        DataGrid1.ItemsSource = _listhisto
+        AddHandler DataGrid1.Loaded, AddressOf GridOK
+    End Sub
+
+    Private Sub GridOk(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        DataGrid1.Columns.RemoveAt(0)
+        DataGrid1.Columns.RemoveAt(0)
+        DataGrid1.Columns.RemoveAt(0)
+    End Sub
+
+    Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
+        RaiseEvent CloseMe(Me)
+    End Sub
+End Class
