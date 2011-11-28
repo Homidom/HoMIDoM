a2be1048a9263590b0f1949135f7a95188a97e2e
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2011-11-28 22:11:09 +0000

    Correction d'une erreur dans la query sqllite (ne renvoyait pas la premiere ligne) COrrection d'un plantage possible si liste de relevevide dans l'admin

--- HoMIAdmiN/HoMIAdmiN/Control/uReleve.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uReleve.xaml.vb
@@ -5,22 +5,29 @@ Public Class uReleve
     Public Event CloseMe(ByVal MyObject As Object)
 
     Public Sub New(ByVal ListReleve As Object, ByVal Label As String)
+        Try
+            ' Cet appel est requis par le concepteur.
+            InitializeComponent()
 
-        ' Cet appel est requis par le concepteur.
-        InitializeComponent()
+            ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+            _listhisto = ListReleve
+            DataGrid1.ItemsSource = _listhisto
+            AddHandler DataGrid1.Loaded, AddressOf GridOk
 
-        ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
-        _listhisto = ListReleve
-        DataGrid1.ItemsSource = _listhisto
-        AddHandler DataGrid1.Loaded, AddressOf GridOk
-
-        LblDev.Content = Label
+            LblDev.Content = Label
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors de l'affichage du relevé: " & ex.ToString, "Erreur Admin", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
     End Sub
 
     Private Sub GridOk(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
-        DataGrid1.Columns.RemoveAt(0)
-        DataGrid1.Columns.RemoveAt(0)
-        DataGrid1.Columns.RemoveAt(0)
+        Try
+            If DataGrid1.Columns.Count > 0 Then DataGrid1.Columns.RemoveAt(0)
+            If DataGrid1.Columns.Count > 0 Then DataGrid1.Columns.RemoveAt(0)
+            If DataGrid1.Columns.Count > 0 Then DataGrid1.Columns.RemoveAt(0)
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors de l'affichage du relevé GridOK: " & ex.ToString, "Erreur Admin", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
     End Sub
 
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
