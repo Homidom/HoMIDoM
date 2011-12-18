b64ecfeea9c1bf5381b807281f57f318ce4fae80
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2011-12-18 08:22:54 +0000

    MaJ Phidget

--- trunk/Driver_Phidget/Driver_Phidget.vb
+++ trunk/Driver_Phidget/Driver_Phidget.vb
@@ -284,7 +284,7 @@ Public Class Driver_Phidget
         If _IsConnect = False Then Exit Sub
         Try
             Select Case Objet.Type
-                Case HoMIDom.HoMIDom.Device.ListeDevices.APPAREIL Or HoMIDom.HoMIDom.Device.ListeDevices.GENERIQUEBOOLEEN
+                Case 1 Or 12 'APPAREIL Or GENERIQUEBOOLEAN
                     If Commande = "ON" Then
                         phidgetIFK.outputs(Objet.Adresse1) = True
                     End If
