d334e277f65a896cf9904b2bd30e7d7f0d830acb
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-01-19 14:09:19 +0000

    MaJ mineures Admin (Ergo & optimisation)

--- trunk/HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
+++ trunk/HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
@@ -42,77 +42,9 @@ Partial Public Class uLog
         ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
         RefreshLog()
 
-        If IsConnect = True Then
-            Dim _list As List(Of Boolean) = myService.GetTypeLogEnable
-            ChkTyp0.IsChecked = _list.Item(0)
-            ChkTyp1.IsChecked = _list.Item(1)
-            ChkTyp2.IsChecked = _list.Item(2)
-            ChkTyp3.IsChecked = _list.Item(3)
-            ChkTyp4.IsChecked = _list.Item(4)
-            ChkTyp5.IsChecked = _list.Item(5)
-            ChkTyp6.IsChecked = _list.Item(6)
-            ChkTyp7.IsChecked = _list.Item(7)
-            ChkTyp8.IsChecked = _list.Item(8)
-            ChkTyp9.IsChecked = _list.Item(9)
-        End If
     End Sub
 
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
-        If IsConnect = True Then
-            Dim _list As New List(Of Boolean)
-            If ChkTyp0.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp1.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp2.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp3.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp4.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp5.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp6.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp7.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp8.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-            If ChkTyp9.IsChecked Then
-                _list.Add(True)
-            Else
-                _list.Add(False)
-            End If
-
-            myService.SetTypeLogEnable(_list)
-        End If
         RaiseEvent CloseMe(Me)
     End Sub
 
