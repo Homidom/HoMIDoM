1117369625b2c0d0f7d3312a77e3d64d3cdaa6a5
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-01-26 12:20:15 +0000

    Amélioration lisibilité du log

--- HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
@@ -47,76 +47,67 @@ Partial Public Class uLog
         RaiseEvent CloseMe(Me)
     End Sub
 
-    'Private Sub DGW_LoadingRow(ByVal sender As Object, ByVal e As System.Windows.Controls.DataGridRowEventArgs) Handles DGW.LoadingRow
-    '    Try
+    Private Sub DGW_LoadingRow(ByVal sender As Object, ByVal e As System.Windows.Controls.DataGridRowEventArgs) Handles DGW.LoadingRow
+        Try
+            Dim RowDataContaxt As System.Data.DataRowView = TryCast(e.Row.DataContext, System.Data.DataRowView)
+            If RowDataContaxt IsNot Nothing Then
+                If RowDataContaxt.Row.IsNull(1) = False Then
+                    Select Case RowDataContaxt.Row.Item(1).ToString
+                        Case "1"
+                            RowDataContaxt.Row.Item(1) = "INFO"
+                            e.Row.Background = Brushes.White
+                        Case "2"
+                            RowDataContaxt.Row.Item(1) = "ACTION"
+                            e.Row.Background = Brushes.White
+                        Case "3"
+                            RowDataContaxt.Row.Item(1) = "MESSAGE"
+                            e.Row.Background = Brushes.White
+                        Case "4"
+                            RowDataContaxt.Row.Item(1) = "VALEUR CHANGE"
+                            e.Row.Background = Brushes.White
+                        Case "5"
+                            RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE"
+                            e.Row.Background = Brushes.White
+                        Case "6"
+                            RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE PRECISION"
+                            e.Row.Background = Brushes.White
+                        Case "7"
+                            RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE LASTETAT"
+                            e.Row.Background = Brushes.White
+                        Case "8"
+                            RowDataContaxt.Row.Item(1) = "ERREUR"
+                            e.Row.Background = Brushes.Red
+                        Case "9"
+                            RowDataContaxt.Row.Item(1) = "ERREUR CRITIQUE"
+                            e.Row.Background = Brushes.Red
+                        Case "10"
+                            RowDataContaxt.Row.Item(1) = "DEBUG"
+                            e.Row.Background = Brushes.Yellow
+                    End Select
 
-    '        If e IsNot Nothing Then
-    '            If e.Row IsNot Nothing Then
-    '                If e.Row.DataContext IsNot Nothing Then
-    '                    '                        If e.Row.Item IsNot Nothing Then
-    '                        Dim RowDataContaxt As System.Data.DataRowView = e.Row.DataContext
-    '                        If RowDataContaxt IsNot Nothing Then
-    '                            If RowDataContaxt.Row IsNot Nothing Then
-    '                            If RowDataContaxt.Row.ItemArray IsNot Nothing Then
-    '                                Select Case RowDataContaxt.Row.Item(1).ToString
-    '                                    Case "1"
-    '                                        RowDataContaxt.Row.Item(1) = "INFO"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "2"
-    '                                        RowDataContaxt.Row.Item(1) = "ACTION"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "3"
-    '                                        RowDataContaxt.Row.Item(1) = "MESSAGE"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "4"
-    '                                        RowDataContaxt.Row.Item(1) = "VALEUR CHANGE"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "5"
-    '                                        RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "6"
-    '                                        RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE PRECISION"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "7"
-    '                                        RowDataContaxt.Row.Item(1) = "VALEUR INCHANGE LASTETAT"
-    '                                        'e.Row.Background = Brushes.White
-    '                                    Case "8"
-    '                                        RowDataContaxt.Row.Item(1) = "ERREUR"
-    '                                        'e.Row.Background = Brushes.Red
-    '                                    Case "9"
-    '                                        RowDataContaxt.Row.Item(1) = "ERREUR CRITIQUE"
-    '                                        'e.Row.Background = Brushes.Red
-    '                                    Case "10"
-    '                                        RowDataContaxt.Row.Item(1) = "DEBUG"
-    '                                        'e.Row.Background = Brushes.Yellow
-    '                                End Select
-    '                                Select Case RowDataContaxt.Row.Item(2).ToString
-    '                                    Case "1"
-    '                                        RowDataContaxt.Row.Item(2) = "SERVEUR"
-    '                                    Case "2"
-    '                                        RowDataContaxt.Row.Item(2) = "SCRIPT"
-    '                                    Case "3"
-    '                                        RowDataContaxt.Row.Item(2) = "TRIGGER"
-    '                                    Case "4"
-    '                                        RowDataContaxt.Row.Item(2) = "DEVICE"
-    '                                    Case "5"
-    '                                        RowDataContaxt.Row.Item(2) = "DRIVER"
-    '                                    Case "6"
-    '                                        RowDataContaxt.Row.Item(2) = "SOAP"
-    '                                    Case "7"
-    '                                        RowDataContaxt.Row.Item(2) = "CLIENT"
-    '                                End Select
-    '                            End If
-    '                            End If
-    '                        '                           End If
-    '                    End If
-    '                End If
-    '            End If
-    '        End If
-    '    Catch ex As Exception
-    '        MsgBox("Err: " & ex.ToString)
-    '    End Try
-    'End Sub
+                    Select Case RowDataContaxt.Row.Item(2).ToString
+                        Case "1"
+                            RowDataContaxt.Row.Item(2) = "SERVEUR"
+                        Case "2"
+                            RowDataContaxt.Row.Item(2) = "SCRIPT"
+                        Case "3"
+                            RowDataContaxt.Row.Item(2) = "TRIGGER"
+                        Case "4"
+                            RowDataContaxt.Row.Item(2) = "DEVICE"
+                        Case "5"
+                            RowDataContaxt.Row.Item(2) = "DRIVER"
+                        Case "6"
+                            RowDataContaxt.Row.Item(2) = "SOAP"
+                        Case "7"
+                            RowDataContaxt.Row.Item(2) = "CLIENT"
+                    End Select
+                End If
+            End If
+
+        Catch ex As Exception
+            MsgBox("Err: " & ex.ToString)
+        End Try
+    End Sub
 
 
 End Class
