e9dd29576a821e4ab091458ef177017ae0b303a6
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-02-28 10:47:17 +0000

    MaJ k8055

--- trunk/Driver_k8055/Driver_k8055.vb
+++ trunk/Driver_k8055/Driver_k8055.vb
@@ -334,9 +334,11 @@ Imports HoMIDom.HoMIDom.Device
                 If Command = "" Then
                     Return False
                 Else
-                    'Write(deviceobject, Command, Param(0), Param(1))
                     Select Case UCase(Command)
                         Case "VALEUR"
+                            For i As Integer = 0 To Param.Length - 1
+                                _Server.Log(TypeLog.DEBUG, TypeSource.DRIVER, Me.Nom & " ExecuteCommand", "Param (" & i & ") : " & Param(i))
+                            Next
                             Write(MyDevice, Command, Param(1), Param(2))
                         Case Else
                     End Select
