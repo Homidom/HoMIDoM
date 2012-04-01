d0cb2729f535fb1abce9c31d8fa23812cea1c383
mc.schmer@gmail.com <mc.schmer@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-04-01 22:56:52 +0000

    Ajout dossier projet pour dev télécommande

---
+++ trunk/Telecommande/Telecommande/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Class MainWindow
+
+    Private Sub button_Click(sender As Object, e As System.Windows.RoutedEventArgs) Handles button.Click
+        Dim img1 As New Image
+        Dim bi1 As New BitmapImage
+        Static Dim state As Boolean
+
+        bi1.BeginInit()
+        bi1.UriSource = New Uri("\Images\1.png", UriKind.Relative)
+        bi1.EndInit()
+        img1.Source = bi1
+
+        If state = False Then
+            grid.SetColumn(img1, 2)
+            grid.SetRow(img1, 1)
+            grid_Telecommande.Children.Add(img1)
+            state = True
+        Else
+            Me.grid_Telecommande.Children.Remove(img1)
+            state = False
+        End If
+        Me.Content = grid_Telecommande
+    End Sub
+End Class
