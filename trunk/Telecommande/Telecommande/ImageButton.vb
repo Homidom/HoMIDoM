48358e61dfb35da50287f753b0013154164fa457
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-04-03 15:52:21 +0000

    fichier manquant

---
+++ trunk/Telecommande/Telecommande/ImageButton.vb
@@ -0,0 +1,24 @@
+﻿Public Class ImageButton
+    Inherits Image
+
+    Dim _Row As Integer
+    Dim _Column As Integer
+
+    Public Property Row As Integer
+        Get
+            Return _Row
+        End Get
+        Set(ByVal value As Integer)
+            _Row = value
+        End Set
+    End Property
+
+    Public Property Column As Integer
+        Get
+            Return _Column
+        End Get
+        Set(ByVal value As Integer)
+            _Column = value
+        End Set
+    End Property
+End Class
