32bd071cbb9993eab09603f6d61d8e5a2672e18c
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-06-24 08:37:06 +0000

    Correction bug lors de l'affichage du log dans l'admin si la description comporte trop de caractères

--- trunk/HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
+++ trunk/HoMIAdmiN/HoMIAdmiN/Control/uLog.xaml.vb
@@ -17,42 +17,51 @@ Partial Public Class uLog
             Me.Cursor = Cursors.Wait
 
             If IsConnect = True Then
-                'If File.Exists("log.txt") Then
-                '    File.Delete("log.txt")
-                'End If
-
                 Dim TargetFile As StreamWriter
                 TargetFile = New StreamWriter("log.txt", False)
                 TargetFile.Write(myservice.ReturnLog)
                 TargetFile.Close()
 
-                'Dim ds As DataSet = New DataSet("Table")
-                'ds.ReadXml("log.txt")
-                'DGW.ItemsSource = ds.Tables(0).DefaultView
-
                 Dim tr As TextReader = New StreamReader("log.txt")
                 Dim lineCount As Integer = 1
 
                 While tr.Peek() >= 0
-                    Dim line As String = tr.ReadLine()
+                    Try
 
-                    Dim data As String() = line.Trim.Split(vbTab)
+                        Dim line As String = tr.ReadLine()
 
-                    ' creates a dictionary where the column name is the key and
-                    '    and the data is the value
-                    Dim sensorData As New Dictionary(Of String, Object)
+                        Dim tmp As String() = line.Trim.Split(vbTab)
+                        Dim data As String()=Nothing
 
-                    For i As Integer = 0 To data.Length - 1
-                        sensorData(keys(i)) = data(i)
-                    Next
+                        If tmp.Length < 6 Then
+                            If tmp(4).Length > 255 Then tmp(4) = Mid(tmp(4), 1, 255)
+                            data = tmp
+                        Else
+                            MessageBox.Show("Une ligne a été supprimée du log car elle ne respecte pas le format: " & tmp.Length)
+                        End If
 
-                    ligneLog.Add(sensorData)
-                    sensorData = Nothing
-                    lineCount += 1
+                        ' creates a dictionary where the column name is the key and
+                        '    and the data is the value
+                        Dim sensorData As New Dictionary(Of String, Object)
+
+                        For i As Integer = 0 To data.Length - 1
+                            sensorData(keys(i)) = data(i)
+                        Next
+
+                        ligneLog.Add(sensorData)
+                        sensorData = Nothing
+                        lineCount += 1
+                    Catch ex As Exception
+                        ' MessageBox.Show("Erreur lors de la ligne du fichier log: " & ex.ToString)
+                    End Try
                 End While
             End If
 
-            DGW.DataContext = ligneLog
+            Try
+                DGW.DataContext = ligneLog
+            Catch ex As Exception
+                MessageBox.Show("Erreur: " & ex.ToString)
+            End Try
 
             'Supression du fichier temporaire
             Me.Cursor = Nothing
@@ -62,24 +71,32 @@ Partial Public Class uLog
     End Sub
 
     Private Sub CreateGridColumn(ByVal headerText As String)
-        Dim col1 As DataGridTextColumn = New DataGridTextColumn()
-        col1.Header = headerText
-        col1.Binding = New Binding(String.Format("[{0}]", headerText))
-        DGW.Columns.Add(col1)
+        Try
+            Dim col1 As DataGridTextColumn = New DataGridTextColumn()
+            col1.Header = headerText
+            col1.Binding = New Binding(String.Format("[{0}]", headerText))
+            DGW.Columns.Add(col1)
+        Catch ex As Exception
+            MessageBox.Show("Erreur: " & ex.ToString)
+        End Try
     End Sub
 
     Public Sub New()
+        Try
 
-        ' Cet appel est requis par le Concepteur Windows Form.
-        InitializeComponent()
+            ' Cet appel est requis par le Concepteur Windows Form.
+            InitializeComponent()
 
-        ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
-        For Each h As String In headers
-            CreateGridColumn(h)
-            keys.Add(h)
-        Next
+            ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+            For Each h As String In headers
+                CreateGridColumn(h)
+                keys.Add(h)
+            Next
 
-        RefreshLog()
+            RefreshLog()
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors sur la fonction New de uLog: " & ex.ToString)
+        End Try
     End Sub
 
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
@@ -124,24 +141,6 @@ Partial Public Class uLog
                         'RowDataContaxt.Row.Item(1) = "DEBUG"
                         e.Row.Background = Brushes.Yellow
                 End Select
-
-                'Select Case RowDataContaxt.Row.Item(2).ToString
-                '    Case "1"
-                '        RowDataContaxt.Row.Item(2) = "SERVEUR"
-                '    Case "2"
-                '        RowDataContaxt.Row.Item(2) = "SCRIPT"
-                '    Case "3"
-                '        RowDataContaxt.Row.Item(2) = "TRIGGER"
-                '    Case "4"
-                '        RowDataContaxt.Row.Item(2) = "DEVICE"
-                '    Case "5"
-                '        RowDataContaxt.Row.Item(2) = "DRIVER"
-                '    Case "6"
-                '        RowDataContaxt.Row.Item(2) = "SOAP"
-                '    Case "7"
-                '        RowDataContaxt.Row.Item(2) = "CLIENT"
-                'End Select
-                'End If
             End If
 
         Catch ex As Exception
