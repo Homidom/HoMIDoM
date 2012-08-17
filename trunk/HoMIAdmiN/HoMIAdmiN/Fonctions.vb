3643b127ea3a90264209b7785c433770053e8f22
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-08-17 16:28:19 +0000

    Admin: Vérifie que le serveur est disponible avant de s'y connecter

--- trunk/HoMIAdmiN/HoMIAdmiN/Fonctions.vb
+++ trunk/HoMIAdmiN/HoMIAdmiN/Fonctions.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
 
 Module Fonctions
     Public Function ConvertArrayToImage(ByVal value As Object) As Object
@@ -12,6 +14,19 @@ Module Fonctions
             ImgSource.EndInit()
         End If
         Return ImgSource
+    End Function
+
+    Public Function UrlIsValid(ByVal Host As String) As Boolean
+        Dim bValid As Boolean = False
+        Try
+            Dim url As New Uri(Host)
+            Dim Request As HttpWebRequest = CType(HttpWebRequest.Create(url), System.Net.HttpWebRequest)
+            Dim response As Net.HttpWebResponse = CType(Request.GetResponse(), Net.HttpWebResponse)
+            bValid = True
+        Catch ex As Exception
+            bValid = False
+        End Try
+        Return bValid
     End Function
 
 End Module
