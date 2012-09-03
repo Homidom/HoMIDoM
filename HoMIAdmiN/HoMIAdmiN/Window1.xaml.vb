68e3ea7b8743582ba6d340f7838b53815da92a8e
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-09-03 09:15:43 +0000

    Amélioration et correction de la gestion des zones dans Admin

--- HoMIAdmiN/HoMIAdmiN/Window1.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Window1.xaml.vb
@@ -250,6 +250,7 @@ Class Window1
                                 MessageBox.Show("Erreur lors de l'enregistrement veuillez consulter le log", "HomIAdmin", MessageBoxButton.OK, MessageBoxImage.Error)
                             Else
                                 MessageBox.Show("Enregistrement effectué", "HomIAdmin", MessageBoxButton.OK, MessageBoxImage.Information)
+                                FlagChange = False
                             End If
                         End If
                     Catch ex As Exception
