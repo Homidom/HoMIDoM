a71c55c92487d4f54693b659ced6b5db407c25c4
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-09-14 10:31:07 +0000

    Nettoyage ZGraph

--- HoMIAdmiN/HoMIAdmiN/Control/uConfigServer.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uConfigServer.xaml.vb
@@ -115,16 +115,6 @@ Partial Public Class uConfigServer
 
                 myService.SetTypeLogEnable(_list)
 
-                For i As Integer = 0 To ListBox1.Items.Count - 1
-                    Dim x As CheckBox = ListBox1.Items(i)
-                    myService.EnableRepertoireAudio(IdSrv, x.Content, x.IsChecked)
-                Next
-
-                For i As Integer = 0 To ListBox2.Items.Count - 1
-                    Dim x As CheckBox = ListBox2.Items(i)
-                    myService.EnableExtensionAudio(IdSrv, x.Content, x.IsChecked)
-                Next
-
                 FlagChange = True
             End If
             RaiseEvent CloseMe(Me)
@@ -184,11 +174,8 @@ Partial Public Class uConfigServer
                 ChkTyp8.IsChecked = _list.Item(8)
                 ChkTyp9.IsChecked = _list.Item(9)
 
-                RefreshList()
             End If
 
-            BtnDelRepertoireAudio.IsEnabled = False
-            BtnDeleteExtension.IsEnabled = False
         Catch ex As Exception
             MessageBox.Show("ERREUR Sub uconfigserver New: " & ex.Message, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
         End Try
@@ -208,123 +195,5 @@ Partial Public Class uConfigServer
         End If
     End Sub
 
-    Private Sub RefreshList()
-        Try
-            ListBox1.Items.Clear()
-            For i As Integer = 0 To myService.GetAllRepertoiresAudio(IdSrv).Count - 1
-                Dim a As New CheckBox
-                a.Content = myService.GetAllRepertoiresAudio(IdSrv).Item(i).Repertoire
-                a.IsChecked = myService.GetAllRepertoiresAudio(IdSrv).Item(i).Enable
-                ListBox1.Items.Add(a)
-                BtnDelRepertoireAudio.IsEnabled = True
-            Next
 
-            ListBox2.Items.Clear()
-            For i As Integer = 0 To myService.GetAllExtensionsAudio(IdSrv).Count - 1
-                Dim a As New CheckBox
-                a.Content = myService.GetAllExtensionsAudio(IdSrv).Item(i).Extension
-                a.IsChecked = myService.GetAllExtensionsAudio(IdSrv).Item(i).Enable
-                ListBox2.Items.Add(a)
-                BtnDeleteExtension.IsEnabled = True
-            Next
-
-
-
-        Catch ex As Exception
-            MessageBox.Show("ERREUR Sub uconfigaudio RefreshList: " & ex.Message, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
-        End Try
-    End Sub
-
-    Private Sub BtnDelRepertoireAudio_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnDelRepertoireAudio.Click
-        Try
-            If ListBox1.SelectedIndex < 0 Then
-                MessageBox.Show("Veuillez un répertoire dans la liste!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-                Exit Sub
-            Else
-                myService.DeleteRepertoireAudio(IdSrv, ListBox1.SelectedItem.content)
-                RefreshList()
-            End If
-        Catch ex As Exception
-            MessageBox.Show("ERREUR Sub uconfigaudio BtnDelRepertoireAudio_Click: " & ex.Message, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
-        End Try
-    End Sub
-
-    Private Sub BtnNewRepertoire_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnNewRepertoire.Click
-        Dim dlg As New Forms.FolderBrowserDialog()
-
-        ' Ouverture de la fenetre de selection d un repertoire
-        Dim retour As Forms.DialogResult = dlg.ShowDialog()
-
-        If dlg.SelectedPath = "" Then
-            MessageBox.Show("Le chemin du répertoire audio ne peut être vide!", "Repertoire Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-            Exit Sub
-        Else
-            If myService.NewRepertoireAudio(IdSrv, dlg.SelectedPath, False) = -1 Then
-                MessageBox.Show("Le chemin du répertoire audio existe déjà il ne sera pas pris en compte", "Repertoire Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-                Exit Sub
-            Else
-                RefreshList()
-            End If
-
-        End If
-    End Sub
-
-    Private Sub BtnNewExension_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnNewExension.Click
-        Dim retour As String
-        retour = InputBox("Veuillez saisir la nouvelle extension Audio sans le point, puis l'activer si besoin par la suite", "Extension Audio", "")
-        If retour = "" Then
-            MessageBox.Show("L'extension audio ne peut être vide!", "Extension Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-            Exit Sub
-        End If
-        If InStr(retour, ".") > 0 Then
-            MessageBox.Show("Veuillez ne pas saisir le . !", "Extension Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-            Exit Sub
-        End If
-        If retour.Length <> 3 Then
-            MessageBox.Show("L'extension doit comporter 3 caractères", "Extension Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-            Exit Sub
-        End If
-        retour = "." & LCase(retour)
-        If myService.NewExtensionAudio(IdSrv, retour, False) = -1 Then
-            MessageBox.Show("L'extension audio existe déjà elle ne sera pas pris en compte", "Extension Audio", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-            Exit Sub
-        Else
-            RefreshList()
-        End If
-
-    End Sub
-
-    Private Sub BtnDeleteExtension_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnDeleteExtension.Click
-        Try
-            If ListBox2.SelectedIndex < 0 Then
-                MessageBox.Show("Veuillez une extension dans la liste!", "Erreur", MessageBoxButton.OK, MessageBoxImage.Exclamation)
-                Exit Sub
-            Else
-                myService.DeleteExtensionAudio(IdSrv, ListBox2.SelectedItem.content)
-                RefreshList()
-            End If
-        Catch ex As Exception
-            MessageBox.Show("ERREUR Sub uconfigaudio BtnDeleteExtension_Click: " & ex.Message, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
-        End Try
-    End Sub
-
-    Private Sub ListBox1_SelectionChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.SelectionChangedEventArgs) Handles ListBox1.SelectionChanged
-        If ListBox1.SelectedItem IsNot Nothing Then
-            BtnDelRepertoireAudio.IsEnabled = True
-        Else
-            BtnDelRepertoireAudio.IsEnabled = False
-        End If
-    End Sub
-
-    Private Sub ListBox2_SelectionChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.SelectionChangedEventArgs) Handles ListBox2.SelectionChanged
-        If ListBox2.SelectedItem IsNot Nothing Then
-            BtnDeleteExtension.IsEnabled = True
-        Else
-            BtnDeleteExtension.IsEnabled = False
-        End If
-    End Sub
-
-    Private Sub UserControl_Loaded(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
-
-    End Sub
 End Class
