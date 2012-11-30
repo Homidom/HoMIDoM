9f68d0da4f6ed8f0dade2b8892b1e17b7adbcb5a
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2012-11-30 09:26:08 +0000

    Update des logs Drivers GSM et Zwave Ajout de try/catch dans le driver zwave

--- trunk/HoMIDom/My Project/AssemblyInfo.vb
+++ trunk/HoMIDom/My Project/AssemblyInfo.vb
@@ -31,9 +31,9 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.249.0")> 
+<Assembly: AssemblyVersion("1.0.250.0")> 
 '<Assembly: AssemblyFileVersion("1.0.0")> 
 
 '<Assembly: AssemblyVersion("1.0.19.0")> 
 '<Assembly: AssemblyFileVersion("1.0.0")> 
-<Assembly: AssemblyFileVersionAttribute("1.0.249.0")> 
+<Assembly: AssemblyFileVersionAttribute("1.0.250.0")> 
