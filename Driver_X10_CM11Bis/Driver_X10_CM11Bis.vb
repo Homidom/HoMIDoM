b41adb2caf6c1508b0f1068e28ce94dc4005e519
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2012-12-03 08:02:20 +0000

    MaJ Client WPF

--- Driver_X10_CM11Bis/Driver_X10_CM11Bis.vb
+++ Driver_X10_CM11Bis/Driver_X10_CM11Bis.vb
@@ -158,11 +158,12 @@ Public Class Driver_X10_CM11Bis
             _Picture = value
         End Set
     End Property
-    Public Property Port_TCP() As Object Implements HoMIDom.HoMIDom.IDriver.Port_TCP
+
+    Public Property Port_TCP() As String Implements HoMIDom.HoMIDom.IDriver.Port_TCP
         Get
             Return _Port_TCP
         End Get
-        Set(ByVal value As Object)
+        Set(ByVal value As String)
             _Port_TCP = value
         End Set
     End Property
