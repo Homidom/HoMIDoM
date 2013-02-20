419eeb41ac210247b804e45663ee385c83209c87
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-02-20 07:52:50 +0000

    Ajout de la fonction AddHisto

--- HoMIAdmiN/HoMIAdmiN/Control/uNewDevice.xaml.vb
+++ HoMIAdmiN/HoMIAdmiN/Control/uNewDevice.xaml.vb
@@ -5,16 +5,11 @@ Imports System.Collections.ObjectModel
 Public Class uNewDevice
     Public Event CloseMe(ByVal MyObject As Object)
 
-    Dim keys As New List(Of String)
-    Dim headers As String() = {"datetime", "typesource", "source", "fonction", "message"}
-
-
     Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles BtnClose.Click
         RaiseEvent CloseMe(Me)
     End Sub
 
     Public Sub New()
-
         Try
 
             ' Cet appel est requis par le Concepteur Windows Form.
@@ -24,7 +19,7 @@ Public Class uNewDevice
             DGW.ItemsSource = myService.GetAllNewDevice
             AddHandler DGW.Loaded, AddressOf GridOk
 
-            RefreshGrid()
+            'RefreshGrid()
         Catch ex As Exception
             MessageBox.Show("Erreur lors sur la fonction New de uNewDevice: " & ex.ToString, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
         End Try
@@ -39,18 +34,6 @@ Public Class uNewDevice
 
         Catch ex As Exception
             MessageBox.Show("Erreur lors de l'affichage du relevé GridOK: " & ex.ToString, "Erreur Admin", MessageBoxButton.OK, MessageBoxImage.Error)
-        End Try
-    End Sub
-
-
-    Private Sub CreateGridColumn(ByVal headerText As String)
-        Try
-            Dim col1 As DataGridTextColumn = New DataGridTextColumn()
-            col1.Header = headerText
-            col1.Binding = New Binding(String.Format("[{0}]", headerText))
-            DGW.Columns.Add(col1)
-        Catch ex As Exception
-            MessageBox.Show("Erreur Sub CreateGridColumn: " & ex.ToString, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
         End Try
     End Sub
 
