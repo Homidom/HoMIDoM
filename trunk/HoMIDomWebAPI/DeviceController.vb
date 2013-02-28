3fc8987ceb2a4bab97ccae446dfb175d124de61f
Faze1224@gmail.com <Faze1224@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-02-28 10:51:57 +0000

    Ajout d'un projet Web API

---
+++ trunk/HoMIDomWebAPI/DeviceController.vb
@@ -0,0 +1,23 @@
+﻿
+Imports System.Web.Http
+Imports HoMIDom
+Imports HoMIDom.HoMIDom
+
+Public Class DeviceController
+    Inherits BaseHoMIdomController
+
+    Public Function [Get]() As List(Of TemplateDevice)
+        Return HoMIDomAPI.CurrentServer.GetAllDevices(Me.ServerKey)
+    End Function
+
+    Public Function [Get](id As String) As TemplateDevice
+        Return HoMIDomAPI.CurrentServer.ReturnDeviceByID(Me.ServerKey, id)
+    End Function
+
+
+    <HttpGet()>
+    Public Function ExecuteCommand(id As String, command As String) As Boolean
+        HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, New DeviceAction() With {.Nom = command})
+        Return True
+    End Function
+End Class
---
+++ trunk/HoMIDomWebAPI/HoMIDomAPI.vb
@@ -0,0 +1,27 @@
+﻿Imports HoMIDom.HoMIDom
+Imports System.Web.Http.SelfHost
+Imports System.Web.Http
+
+Public Class HoMIDomAPI
+    Private Shared server As HttpSelfHostServer
+
+    Public Shared Property CurrentServer() As IHoMIDom
+
+    Public Shared Property ServerKey() As String
+
+    Public Shared Sub Start(url As String, key As String)
+        ServerKey = key
+        Dim config = New HttpSelfHostConfiguration(url)
+
+        config.Routes.MapHttpRoute("DefaultApi", "api/{key}/{controller}/{id}", New With { _
+         .id = RouteParameter.[Optional] _
+        })
+        config.Routes.MapHttpRoute("CommandApi", "api/{key}/command/{controller}/{id}/{command}", New With { _
+         .action = "ExecuteCommand" _
+        })
+
+        Console.WriteLine(String.Format("{0} Starting Web API on  {1}", Now, url))
+        server = New HttpSelfHostServer(config)
+        server.OpenAsync()
+    End Sub
+End Class
