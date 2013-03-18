5bab0a018105c11831e8cf19ba16871cb93b88c6
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-03-18 22:39:53 +0000

    Correction erreur composant Batterie Correction Bug driver rfxtrx avec composant Blyss Compilations drivers et dlls suites aux nombreuse modifs

--- HoMIAdmiN/HoMIAdmiN/My Project/AssemblyInfo.vb
+++ HoMIAdmiN/HoMIAdmiN/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.90.0")> 
-<Assembly: AssemblyFileVersion("1.0.90.0")> 
+<Assembly: AssemblyVersion("1.0.92.0")> 
+<Assembly: AssemblyFileVersion("1.0.92.0")> 
