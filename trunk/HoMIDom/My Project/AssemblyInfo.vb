b9a616da92de2cfdce365ff2062f2697faa2d076
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-03-28 21:40:22 +0000

    Correction erreur format séparateur dans meteoweather sur clavier français Correction driver CurrentCost avec autocreation de composant

--- trunk/HoMIDom/My Project/AssemblyInfo.vb
+++ trunk/HoMIDom/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.435.0")> 
-<Assembly: AssemblyFileVersionAttribute("1.0.435.0")> 
+<Assembly: AssemblyVersion("1.0.436.0")> 
+<Assembly: AssemblyFileVersionAttribute("1.0.436.0")> 
