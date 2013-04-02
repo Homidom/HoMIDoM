9ae01bebe31aeda88eded4278a8239ba4e437dbd
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-04-02 17:10:19 +0000

    Correction Bug Action Mail

--- trunk/HoMIDom/Mail.vb
+++ trunk/HoMIDom/Mail.vb
@@ -52,7 +52,7 @@ Namespace HoMIDom
                             .Body = DecodeCommand(_texte)
                         End With
                     Catch ex As Exception
-                        _Server.Log(Server.TypeLog.ERREUR, Server.TypeSource.SERVEUR, "SendMail", "Erreur lors de l'envoi du mail: " & ex.Message)
+                        _Server.Log(Server.TypeLog.ERREUR, Server.TypeSource.SERVEUR, "SendMail", "Erreur lors de la préparation du mail: " & ex.ToString)
                     End Try
 
                     With (Smtp)
@@ -63,13 +63,55 @@ Namespace HoMIDom
 
                         .Credentials = New Net.NetworkCredential(_Login, _Password) 'De:Votre Email , Pass: password de l'Email
                         .Send(EmailMessage)
-                        _Server.Log(Server.TypeLog.DEBUG, Server.TypeSource.SERVEUR, "SendMail", "Message envoyé")
+                        _Server.Log(Server.TypeLog.DEBUG, Server.TypeSource.SERVEUR, "SendMail", "Message envoyé, Message=" & EmailMessage.Body)
                     End With
                 End If
             Catch ex As Exception
-                _Server.Log(Server.TypeLog.ERREUR, Server.TypeSource.SERVEUR, "SendMail", "Erreur lors de l'envoi du mail: " & ex.Message)
+                _Server.Log(Server.TypeLog.ERREUR, Server.TypeSource.SERVEUR, "SendMail", "Erreur lors de l'envoi du mail: " & ex.ToString)
             End Try
         End Sub
+
+        Public Function DecodeCommand(ByVal Command As String) As String
+            Dim retour As String = Command
+            Try
+                Dim startcmd As Integer = InStr(1, Command, "<")
+                Dim endcmd As Integer = InStr(1, Command, ">")
+                Dim newcmd As String = Command
+
+                Do While startcmd > 0 And endcmd > 0
+                    Dim _device As String = Mid(newcmd, startcmd + 1, endcmd - startcmd - 1)
+                    Dim Tabl() As String = _device.Split(".")
+
+                    If Tabl.Length = 1 Then
+                        Dim x As Object = _Server.ReturnRealDeviceByName(Tabl(0))
+                        If x IsNot Nothing Then
+                            _device = x.Value
+                        Else
+                            _Server.Log(Server.TypeLog.DEBUG, Server.TypeSource.SCRIPT, "DecodeCommand", "Device: " & Tabl(0) & " non trouvé")
+                        End If
+                    ElseIf Tabl.Length = 2 Then
+                        Dim x As Object = _Server.ReturnRealDeviceByName(Tabl(0))
+                        If x IsNot Nothing Then
+                            Dim value As Object = CallByName(x, Tabl(1), CallType.Get)
+                            _device = value
+                        End If
+                    End If
+
+                    Dim start As String = Mid(newcmd, 1, startcmd - 1)
+                    Dim fin As String = Mid(newcmd, endcmd + 1, newcmd.Length - endcmd)
+                    newcmd = start & _device & fin
+                    retour = newcmd
+                    startcmd = InStr(1, newcmd, "<")
+                    endcmd = InStr(1, newcmd, ">")
+                Loop
+
+                Return retour
+            Catch ex As Exception
+                retour = "Error:" & ex.ToString
+                '_Server.Log(Server.TypeLog.ERREUR, Server.TypeSource.SCRIPT, "DecodeCommand", "Error:" & ex.ToString)
+                Return retour
+            End Try
+        End Function
 
     End Class
 End Namespace
