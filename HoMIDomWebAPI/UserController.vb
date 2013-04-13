2fa185ab132616e18d0a24c67912c6b980d8ddf4
DomoSoftDev@gmail.com <DomoSoftDev@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-04-13 20:40:58 +0000

    WEB API - Ajout des contrôleurs Driver, User et Zone

---
+++ HoMIDomWebAPI/UserController.vb
@@ -0,0 +1,24 @@
+﻿
+Imports System.Web.Http
+Imports HoMIDom
+Imports HoMIDom.HoMIDom
+
+Public Class UserController
+    Inherits BaseHoMIdomController
+
+    Public Function [Get]() As List(Of Users.User)
+        Return HoMIDomAPI.CurrentServer.GetAllUsers(Me.ServerKey)
+    End Function
+
+    Public Function [Get](id As String) As Users.User
+        Return HoMIDomAPI.CurrentServer.ReturnUserByUsername(Me.ServerKey, id)
+    End Function
+
+
+    <HttpGet()>
+    Public Function ExecuteCommand(id As String, command As String) As Boolean
+        HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, New DeviceAction() With {.Nom = command})
+        Return True
+    End Function
+
+End Class
