1eae3207401ddb9d29af9dba33dc22f33d2d9a8e
DomoSoftDev@gmail.com <DomoSoftDev@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-05-01 21:17:51 +0000

    Driver GoogleCalendar - Draft - Read opérationnel

---
+++ trunk/Drivers/Driver_GoogleCalendar/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("ClassLibrary1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ClassLibrary1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("e23a430e-72a0-43a6-a4f6-8b6fc6f19b29")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
