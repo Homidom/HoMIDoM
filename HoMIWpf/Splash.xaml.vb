7dba52dee891f1e980c4c8cf56799bac3a1db845
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-05-28 06:05:49 +0000

    Ajout du SplashScreen dans le client WPF

---
+++ HoMIWpf/Splash.xaml.vb
@@ -0,0 +1,19 @@
+﻿Public Class Window2
+
+    Public Sub New()
+        Try
+            ' Cet appel est requis par le concepteur.
+            InitializeComponent()
+
+            ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+            Label2.Content = My.Application.Info.Title
+            Label1.Content = "Version:" & My.Application.Info.Version.ToString & vbCrLf & My.Application.Info.Copyright
+        Catch ex As Exception
+            MessageBox.Show("ERREUR Sub New Splash() : " & ex.Message, "ERREUR", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+End Class
