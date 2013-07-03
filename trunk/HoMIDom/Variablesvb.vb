d354e01387136edf11a4aa7de41093848bc56042
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-07-03 18:18:58 +0000

    Modification du nouveau service Homidom

--- trunk/HoMIDom/Variablesvb.vb
+++ trunk/HoMIDom/Variablesvb.vb
@@ -2,6 +2,7 @@
 
 Module Variablesvb
     Public _IdSrv As String = "123456789" 'id du serveur, utilisé pour la communication soap
-    Public _MonRepertoire As String = System.Environment.CurrentDirectory 'représente le répertoire de l'application 'Application.StartupPath
+    'Public _MonRepertoire As String = System.Environment.CurrentDirectory 'représente le répertoire de l'application 'Application.StartupPath
+    Public _MonRepertoire As String = My.Application.Info.DirectoryPath 'représente le répertoire de l'application 'Application.StartupPath
 
 End Module
