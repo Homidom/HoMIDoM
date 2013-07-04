a16abe40c0acf8ba3730ec201f58afecfd9b7fe6
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-07-04 17:54:43 +0000

    Envoi des fichiers manquants pour le nouveau service

---
+++ trunk/HoMIServicE/ProjectInstaller.Designer.vb
@@ -0,0 +1,47 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur de composants
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur de composants
+    'Elle peut être modifiée à l'aide du Concepteur de composants.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ServiceProcessInstaller1 = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.ServiceInstaller1 = New System.ServiceProcess.ServiceInstaller()
+        '
+        'ServiceProcessInstaller1
+        '
+        Me.ServiceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem
+        Me.ServiceProcessInstaller1.Password = Nothing
+        Me.ServiceProcessInstaller1.Username = Nothing
+        '
+        'ServiceInstaller1
+        '
+        Me.ServiceInstaller1.Description = "HoMIServicE"
+        Me.ServiceInstaller1.DisplayName = "HoMIServicE"
+        Me.ServiceInstaller1.ServiceName = "HoMIServicE"
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.ServiceProcessInstaller1, Me.ServiceInstaller1})
+
+    End Sub
+    Friend WithEvents ServiceProcessInstaller1 As System.ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents ServiceInstaller1 As System.ServiceProcess.ServiceInstaller
+
+End Class
