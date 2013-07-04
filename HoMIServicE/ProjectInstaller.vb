00d595ee568620c91d6aff494c9be2ed1a57ede1
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-07-04 17:54:43 +0000

    Envoi des fichiers manquants pour le nouveau service

---
+++ HoMIServicE/ProjectInstaller.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration.Install
+
+Public Class ProjectInstaller
+
+    Public Sub New()
+        MyBase.New()
+
+        'Cet appel est requis par le Concepteur de composants.
+        InitializeComponent()
+
+        'Ajoutez le code d'initialisation après l'appel de InitializeComponent
+
+    End Sub
+
+End Class
