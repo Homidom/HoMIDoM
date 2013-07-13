1c502d3884b84858b755cfaaecb52c7eabb1f313
Faze1224@gmail.com <Faze1224@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-07-13 18:32:29 +0000

    Intégration de SignalR dans homidom

---
+++ trunk/HoMIDomWebAPI/SignalR.vb
@@ -0,0 +1,33 @@
+﻿
+Imports Microsoft.AspNet.SignalR
+Imports Microsoft.AspNet.SignalR.Hubs
+Imports Owin
+
+Public Class Startup
+    Public Sub Configuration(ByVal app As IAppBuilder)
+
+        Dim config = New HubConfiguration With {.EnableCrossDomain = True}
+        app.MapHubs(config)
+    End Sub
+End Class
+
+
+<HubName("NotificationHub")> _
+<HubAuthorize()> _
+Public Class NotificationHub
+    Inherits Hub
+    Public Sub Test(param As String)
+        Console.WriteLine(param)
+        Clients.All.test(param)
+    End Sub
+End Class
+
+
+Public Class HubAuthorizeAttribute
+    Inherits Attribute
+    Implements IAuthorizeHubConnection
+
+    Public Function AuthorizeHubConnection(hubDescriptor As HubDescriptor, request As IRequest) As Boolean Implements IAuthorizeHubConnection.AuthorizeHubConnection
+        Return request.Headers("sKey") = HoMIDomAPI.ServerKey
+    End Function
+End Class
