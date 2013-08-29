cfd56d0175f75b99dd2d3171a1ea785ec1c5847e
sebehome@gmail.com <sebehome@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-08-29 16:48:16 +0000

    MaJ mineures

--- Drivers/Driver_X10_CM15/Driver_X10_CM15.vb
+++ Drivers/Driver_X10_CM15/Driver_X10_CM15.vb
@@ -24,7 +24,7 @@ Public Class Driver_X10_CM15
     Dim _Refresh As Integer = 0
     Dim _Modele As String = "CM15"
     Dim _Version As String = My.Application.Info.Version.ToString
-    Dim _OsPlatform As String = "32"
+    Dim _OsPlatform As String = "3264"
     Dim _Picture As String = ""
     Dim _Server As HoMIDom.HoMIDom.Server
     Dim _Device As HoMIDom.HoMIDom.Device
