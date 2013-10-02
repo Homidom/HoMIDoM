a6c26f18afa09a5a05ab7d29a3ddc07f03077cbf
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-10-02 09:11:23 +0000

    Compilation pour test final V1 et correction erreur client wpf

--- trunk/HoMIWpf/My Project/AssemblyInfo.vb
+++ trunk/HoMIWpf/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.10.3")> 
-<Assembly: AssemblyFileVersion("1.0.10.3")> 
+<Assembly: AssemblyVersion("1.0.12.3")> 
+<Assembly: AssemblyFileVersion("1.0.12.3")> 
