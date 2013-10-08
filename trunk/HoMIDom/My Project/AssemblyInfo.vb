b9f19ae27b9487caeee7b17d8651b0737658d3a0
davidinfo05 <davidinfo05@20adc8bb-5283-5848-245b-9f88ff717279>
2013-10-08 22:39:55 +0000

    Correction url serveur remerciement Correction version zwave

--- trunk/HoMIDom/My Project/AssemblyInfo.vb
+++ trunk/HoMIDom/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.9.0")> 
-<Assembly: AssemblyFileVersionAttribute("1.1.9.0")> 
+<Assembly: AssemblyVersion("1.1.10.0")> 
+<Assembly: AssemblyFileVersionAttribute("1.1.10.0")> 
