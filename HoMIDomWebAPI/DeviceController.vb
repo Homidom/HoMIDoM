218344c4e1aad58b0fa276780eea4df262202949
Faze1224@gmail.com <Faze1224@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2013-11-18 13:34:03 +0000

    Web API > Exécution de commande: autoriser le passage de paramètre via l'URL

--- HoMIDomWebAPI/DeviceController.vb
+++ HoMIDomWebAPI/DeviceController.vb
@@ -3,6 +3,7 @@ Imports System.Web.Http
 Imports HoMIDom
 Imports HoMIDom.HoMIDom
 Imports System.Text.RegularExpressions
+Imports System.Net.Http
 
 Public Class DeviceController
     Inherits BaseHoMIdomController
@@ -17,7 +18,9 @@ Public Class DeviceController
 
     <HttpGet()>
     Public Function ExecuteCommand(id As String, command As String) As Boolean
-        HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, New DeviceAction() With {.Nom = command})
+        Me.ExecuteCommandWithParams(id, command, Me.Request.RequestUri.ParseQueryString())
+        
+        ' HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, New DeviceAction() With {.Nom = command})
         Return True
     End Function
 
@@ -34,13 +37,64 @@ Public Class DeviceController
     ''' <param name="parameters">Listes de paramètres à passer à l'action. param1.nom:param1.value;param2.nom:param2.value</param>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    <HttpGet()>
-    Public Function ExecuteCommandWithParams(id As String, command As String, parameters As String) As Boolean
+    '<HttpGet()>
+    'Public Function ExecuteCommandWithParams(id As String, command As String, parameters As String) As Boolean
 
-        Dim action As DeviceAction
-        action = New DeviceAction() With {.Nom = command}
+    '    Dim action As DeviceAction
+    '    action = New DeviceAction() With {.Nom = command}
 
-        If Not String.IsNullOrEmpty(parameters) Then
+    '    If Not String.IsNullOrEmpty(parameters) Then
+
+    '        ' récupération du composant
+    '        Dim tplDevice As TemplateDevice = HoMIDomAPI.CurrentServer.ReturnDeviceByID(Me.ServerKey, id)
+    '        If tplDevice Is Nothing Then Throw New ArgumentException("Composant non trouvé !", "id")
+
+    '        ' recherche de l'action invoquée
+    '        Dim devAction As DeviceAction = tplDevice.DeviceAction.Where(Function(t) t.Nom = command).FirstOrDefault()
+    '        If devAction Is Nothing Then Throw New ArgumentException("Action non trouvée pour ce composant !", "command")
+
+    '        ' extraction des paramètre reçu
+    '        Dim params As String()
+    '        params = Regex.Split(parameters, ";")
+
+    '        ' assignation des paramètres à l'action
+    '        For Each param As String In params
+
+    '            Dim paramKV As String()
+    '            paramKV = Regex.Split(param, ":")
+
+    '            ' vérification de la présente d'une valeur pour la paramètre 
+    '            If paramKV.Length > 1 Then
+    '                Dim devActionParamater As DeviceAction.Parametre = devAction.Parametres.Where(Function(t) t.Nom = paramKV(0)).FirstOrDefault()
+    '                If Not devActionParamater Is Nothing Then
+    '                    Select Case devActionParamater.Type.ToLower()
+    '                        Case "int32"
+    '                            Dim intVal As Integer
+    '                            If Integer.TryParse(paramKV(1), intVal) Then devActionParamater.Value = intVal
+    '                    End Select
+
+    '                    If Not devActionParamater.Value Is Nothing Then
+    '                        action.Parametres.Add(devActionParamater)
+    '                    End If
+    '                End If
+    '            End If
+
+    '        Next
+
+    '    End If
+
+    '    HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, action)
+    '    Return True
+    'End Function
+
+
+
+
+    Private Function ExecuteCommandWithParams(id As String, command As String, parameters As System.Collections.Specialized.NameValueCollection) As Boolean
+
+        Dim action As DeviceAction = New DeviceAction() With {.Nom = command}
+
+        If Not parameters Is Nothing And parameters.Count > 0 Then
 
             ' récupération du composant
             Dim tplDevice As TemplateDevice = HoMIDomAPI.CurrentServer.ReturnDeviceByID(Me.ServerKey, id)
@@ -50,28 +104,21 @@ Public Class DeviceController
             Dim devAction As DeviceAction = tplDevice.DeviceAction.Where(Function(t) t.Nom = command).FirstOrDefault()
             If devAction Is Nothing Then Throw New ArgumentException("Action non trouvée pour ce composant !", "command")
 
-            ' extraction des paramètre reçu
-            Dim params As String()
-            params = Regex.Split(parameters, ";")
-
-            ' assignation des paramètres à l'action
-            For Each param As String In params
-
-                Dim paramKV As String()
-                paramKV = Regex.Split(param, ":")
-
+            For Each pKey In parameters.AllKeys
+                Dim parameterKey = pKey 'Utile pour l'expression lambda plus bas
+                Dim parameterValue = parameters(parameterKey)
                 ' vérification de la présente d'une valeur pour la paramètre 
-                If paramKV.Length > 1 Then
-                    Dim devActionParamater As DeviceAction.Parametre = devAction.Parametres.Where(Function(t) t.Nom = paramKV(0)).FirstOrDefault()
-                    If Not devActionParamater Is Nothing Then
-                        Select Case devActionParamater.Type.ToLower()
+                If Not String.IsNullOrEmpty(parameterValue) Then
+                    Dim devActionParameter As DeviceAction.Parametre = devAction.Parametres.Where(Function(t) t.Nom = parameterKey).FirstOrDefault()
+                    If Not devActionParameter Is Nothing Then
+                        Select Case devActionParameter.Type.ToLower()
                             Case "int32"
                                 Dim intVal As Integer
-                                If Integer.TryParse(paramKV(1), intVal) Then devActionParamater.Value = intVal
+                                If Integer.TryParse(parameterValue, intVal) Then devActionParameter.Value = intVal
                         End Select
 
-                        If Not devActionParamater.Value Is Nothing Then
-                            action.Parametres.Add(devActionParamater)
+                        If Not devActionParameter.Value Is Nothing Then
+                            action.Parametres.Add(devActionParameter)
                         End If
                     End If
                 End If
@@ -83,6 +130,5 @@ Public Class DeviceController
         HoMIDomAPI.CurrentServer.ExecuteDeviceCommand(Me.ServerKey, id, action)
         Return True
     End Function
-
 
 End Class
