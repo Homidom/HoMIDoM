8e8a92b6ac8c6aa8cb7bfb9d36f2c30b4c372c07
DomoSoftDev@gmail.com <DomoSoftDev@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2014-07-19 11:34:33 +0000

    Ré-ecriture de l'application avec Biblio knockout 3.1.0 - Integration de la page de login - Ajout message d'info, valeur par defaut (port, Idserver) - Gestion connexion auto avec serveur par defaut - Amelioration de la gestion des messages d'erreur - Integration knockout sur les zones, macros

--- HoMIWeB/laurent/js/menulogin.js
+++ HoMIWeB/laurent/js/menulogin.js
@@ -4,62 +4,74 @@
 
 // enregistre les informations d'un site
 function ClickBouton(){
+
+	zones.removeAll();
+	macros.removeAll();
+	
 	// Lancement de l'application 
 	 if (!window.sessionStorage){
         return;
 	 }
-	//alert("Lancement de l'application"+ SiteActuel);
+	// alert("Lancement de l'application"+ SiteActuel);
 	sessionStorage.SiteActuel= SiteActuel;
-	setTimeout(function(){document.location.href = "homiweb.html"; },200);
+
+	ServerName =  ListeServeurs()[SiteActuel].SERVER; 
+	PortActuel = ListeServeurs()[SiteActuel].Port;
+	AdresseIPActuel = ListeServeurs()[SiteActuel].AdresseIP;
+	IDSERVERActuel = ListeServeurs()[SiteActuel].IDSERVER;
+	ConnectServer();
+
 }  
 
 
 // stocke tous les informations de site dans un tableau
 function ReadSites(){
+	var key;
+	// console.log("Passage par ReadSites");
 	// Vide le tableau contenant la definition des sites
  	ListeServeurs.splice(0, ListeServeurs.length);
-	// Supprime tous les icones deja exitantes
-	v_div_parent = document.getElementById("ListeServeurs");
-	while (v_div_parent.firstChild) {
-  		v_div_parent.removeChild(v_div_parent.firstChild);
-}
+	$('#ListeServeurs').empty();
+	
 	if(typeof(Storage)!=="undifined"){
 		var Cpt = localStorage.length;
-	}
-		
-	//alert ("le compteur est de " + Cpt);
-	if  (Cpt)  {
+	}	
 	
-	// REdessine toutes les icones
+	if  (Cpt)  {
+	// Redessine toutes les icones
 		for (i=0;i<Cpt;i++) {
 			key = localStorage.key(i);
+			console.log("Valeurs sauvegardées : "+key);
+		//	ListeServeurs.push(key);
 			InfosServeur = JSON.parse(localStorage.getItem(key));
+			console.log("Valeurs Site: "+i+" "+InfosServeur.SERVER);			
 			ListeServeurs.push(InfosServeur);
-			document.getElementById('ListeServeurs').innerHTML += '<div><img src="Images/icones/Home_128.png" onclick="AfficheSite(' + i +')" /><span>'+ InfosServeur.SERVER + '</span></div>';
-			document.getElementById('ListeServeurs').style.display = 'inline' ;
+		//	$('#ListeServeurs').append('<div><img src="Images/icones/Home_128.png" onclick="AfficheSite(' +  i +')" /><span>'+ InfosServeur.SERVER + '</span></div>');
+			if  (InfosServeur.ServerDef) {
+				AfficheSite(i);
+				ClickBouton();
+			} 
 		}
 	}
-	else 
-	{
-		document.getElementById('ListeServeurs').style.display = 'none' ;
-	}
+	
 }	
 
 // Affiche les infos du site selectionné
 function AfficheSite(IndexSite){
-	// alert("Passage par la fonction alert avec le parametre : "+IndexSite)
-	document.getElementById('ServerName').value = ListeServeurs[IndexSite].SERVER;
-	document.getElementById('ServerID').value = ListeServeurs[IndexSite].IDSERVER;
-	document.getElementById('ServerIP').value = ListeServeurs[IndexSite].AdresseIP;
-	document.getElementById('ServerPort').value = ListeServeurs[IndexSite].Port;
+//	alert("Passage par la fonction AfficheSite avec le parametre : "+IndexSite +" / "+ ListeServeurs()[IndexSite].SERVER);
+	document.getElementById('ServerName').value = ListeServeurs()[IndexSite].SERVER;
+	document.getElementById('ServerID').value = ListeServeurs()[IndexSite].IDSERVER;
+	document.getElementById('ServerIP').value = ListeServeurs()[IndexSite].AdresseIP;
+	document.getElementById('ServerPort').value = ListeServeurs()[IndexSite].Port;
+	$("#ServerDef").prop('checked',ListeServeurs()[IndexSite].ServerDef);
+
 	SiteActuel = IndexSite;
 }
 
 // Affiche les infos du site selectionné
 function SupprimeSite(){
-	if (ListeServeurs.length) {
-		// alert("Passage par la fonction SupprimeSite avec le parametre : "+SiteActuel + " - " +ListeServeurs[SiteActuel].NomSite)  ;	
-		localStorage.removeItem(ListeServeurs[SiteActuel].NomSite);
+	if (ListeServeurs().length) {
+		//alert("Passage par la fonction SupprimeSite avec le parametre : "+SiteActuel + " - " +ListeServeurs()[SiteActuel].NomSite)  ;	
+		localStorage.removeItem(ListeServeurs()[SiteActuel].NomSite);
 		ListeServeurs.splice(SiteActuel,1);	
 		ReadSites();
 	}
@@ -68,9 +80,11 @@ function SupprimeSite(){
 
 // enregistre les informations d'un site
 function AjouterSite(){
+	
 	var InfosServeur = new Object();
 	var storage = window.localStorage;
 
+	// alert("Passage par la fonction AjouterSite avec le parametre : ")
 	if(typeof(Storage)!=="undifined"){
 		var Cpt = storage.length;
 	}
@@ -84,7 +98,9 @@ function AjouterSite(){
 		InfosServeur.SERVER    = document.getElementById('ServerName').value;
 		InfosServeur.IDSERVER  = document.getElementById('ServerID').value;
 		InfosServeur.AdresseIP = document.getElementById('ServerIP').value;
-		InfosServeur.Port      = document.getElementById('ServerPort').value;	
+		InfosServeur.Port      = document.getElementById('ServerPort').value;
+		InfosServeur.ServerDef = $('#ServerDef').is(":checked");
+
 		localStorage.setItem(NomSite, JSON.stringify(InfosServeur));	
 		ReadSites();  	
 	}
