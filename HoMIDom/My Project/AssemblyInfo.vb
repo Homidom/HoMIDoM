df0c64a1cfd132df3c778b013881b5d99e0deeed
davidinfo05@gmail.com <davidinfo05@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2014-11-28 22:53:13 +0000

    Serveur et API web: nettoyage et resolution du pb de system.net.http.dll sur certains OS. Compilation en Release Nouvel installeur 1.2.0.1720

--- HoMIDom/My Project/AssemblyInfo.vb
+++ HoMIDom/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.2")> 
-<Assembly: AssemblyFileVersionAttribute("1.2.0.2")> 
+<Assembly: AssemblyVersion("1.2.2.3")> 
+<Assembly: AssemblyFileVersionAttribute("1.2.2.3")> 
