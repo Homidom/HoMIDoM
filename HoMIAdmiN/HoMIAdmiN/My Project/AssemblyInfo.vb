1977a0b80756327b01ad6596737d6b777bd9372f
davidinfo05@gmail.com <davidinfo05@gmail.com@20adc8bb-5283-5848-245b-9f88ff717279>
2015-01-28 09:23:41 +0000

    Homiadmin: correction erreur clic droit/démarrer un driver

--- HoMIAdmiN/HoMIAdmiN/My Project/AssemblyInfo.vb
+++ HoMIAdmiN/HoMIAdmiN/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.1.3")> 
-<Assembly: AssemblyFileVersion("1.2.1.3")> 
+<Assembly: AssemblyVersion("1.2.1.4")> 
+<Assembly: AssemblyFileVersion("1.2.1.4")> 
